537d27d5a756e897d2bf8936b7b0ebd2fa2dba6d
eleith <eleith@noreply.git.eleith.com>
2021-01-16 18:11:27 -0800

    use async to limit google api calls

--- tools/books-parser.ts
+++ tools/books-parser.ts
@@ -4,45 +4,49 @@ import axios from 'axios'
 import { createWriteStream } from 'fs'
 import { google } from 'googleapis'
 import { config } from 'dotenv'
+import { queue, QueueObject } from 'async'
 
 config()
 
-interface BookSearchDoc {
+interface OpenLibraryBookDoc {
   isbn: Array<string>
   title: string
   author_name: Array<string>
 }
 
-interface BookSearch {
+interface OpenLibrarySearchResponse {
   start: number
   num_found: number
-  docs: Array<BookSearchDoc>
+  docs: Array<OpenLibraryBookDoc>
 }
 
-interface BookCSV {
+interface BookRow {
   title: string
   author: string
   readDate: string
   isbn: string
 }
 
-interface BookParseStatus {
-  scanning: boolean
-  read: number
-  written: number
+interface BookSearchQueue {
+  row: BookRow
+  stream: CsvFormatterStream<Row<BookRow>, Row>
 }
 
-async function findWithOpenLibrary(book: BookCSV): Promise<BookCSV> {
+async function findWithOpenLibrary(book: BookRow): Promise<BookRow> {
   let isbn = book.isbn || ''
   let title = book.title || ''
   let author = book.author || ''
 
-  const response = await axios.get<BookSearch>('http://openlibrary.org/search.json', {
-    params: {
-      title: book.title,
-      author: book.author,
-    },
-  })
+  console.log(`searching open library for '${book.title}'`)
+  const response = await axios.get<OpenLibrarySearchResponse>(
+    'http://openlibrary.org/search.json',
+    {
+      params: {
+        title: book.title,
+        author: book.author,
+      },
+    }
+  )
 
   if (response.status === 200 && response.data.num_found > 0) {
     const bookSearchDocs = response.data.docs[0]
@@ -52,17 +56,18 @@ async function findWithOpenLibrary(book: BookCSV): Promise<BookCSV> {
     title = bookSearchDocs.title
     author = bookSearchDocs.author_name.join(',')
   } else {
-    console.log(`no results found for ${book.title}`)
+    console.log(`no results found for '${book.title}'`)
   }
 
   return { title, author, readDate: book.readDate, isbn }
 }
 
-async function findWithGoogle(book: BookCSV): Promise<BookCSV> {
+async function findWithGoogle(book: BookRow): Promise<BookRow> {
   let isbn = book.isbn || ''
   let title = book.title || ''
   let author = book.author || ''
 
+  console.log(`searching google for '${book.title}'`)
   const response = await googleBooks.volumes.list({ q: `${book.title} ${book.author}` })
 
   if (response.status === 200 && response.data.items?.length) {
@@ -74,59 +79,60 @@ async function findWithGoogle(book: BookCSV): Promise<BookCSV> {
     title = bookSearch.volumeInfo?.title || ''
     author = bookSearch.volumeInfo?.authors?.join(',') || ''
   } else {
-    console.log(`no results found for ${book.title}`)
+    console.log(`no results found for '${book.title}'`)
   }
   return { title, author, readDate: book.readDate, isbn }
 }
 
-async function searchBook(
-  book: BookCSV,
-  stream: CsvFormatterStream<Row, Row>,
-  status: BookParseStatus
-): Promise<void> {
+async function searchBook(book: BookRow, stream: CsvFormatterStream<Row, Row>): Promise<void> {
   let isbn = book.isbn || ''
   let title = book.title || ''
   let author = book.author || ''
 
   if (!isbn) {
-    const bookUpdated = useGoogleSearch
-      ? await findWithGoogle(book)
-      : await findWithOpenLibrary(book)
+    const search = useGoogleSearch ? findWithGoogle : findWithOpenLibrary
+    const bookUpdated = await search(book)
     title = bookUpdated.title
     author = bookUpdated.author
     isbn = bookUpdated.isbn
   }
 
   stream.write({ title, author, readDate: book.readDate, isbn })
-  status.written += 1
-
-  if (!status.scanning && status.read === status.written) {
-    stream.end()
-  }
 }
 
-async function onFinish(rowCount: number): Promise<void> {
-  searchStatus.scanning = false
-  searchStatus.read = rowCount
+async function readFinished(
+  rowCount: number,
+  queue: QueueObject<BookSearchQueue>,
+  stream: CsvFormatterStream<Row, Row>
+): Promise<void> {
   console.log(`done scanning ${rowCount} rows`)
+  queue.drain(function () {
+    console.log('done searching')
+    stream.end()
+  })
 }
 
-const bookCSV = './data/books.csv'
-const bookCSVIsbn = './data/books-searched.csv'
-const writeStream = format()
-const readStream = parseFile(bookCSV, { headers: true, maxRows: 10 })
-const searchStatus: BookParseStatus = {
-  scanning: true,
-  written: 0,
-  read: 0,
+function readRow(row: BookRow): void {
+  searchQueue.push({ row, stream: writeStream }, () =>
+    console.log(`scanned and searched for '${row.title}'`)
+  )
 }
+
+const bookListFile = './data/books.csv'
+const bookUpdatedListFile = './data/books-searched.csv'
+const writeStream = format()
+const readStream = parseFile<Row<BookRow>, Row>(bookListFile, { headers: true })
 const googleBooksApiKey = process.env.GOOGLE_BOOKS_API_KEY
 const googleBooks = google.books({ version: 'v1', auth: googleBooksApiKey })
 const useGoogleSearch = true
+const searchQueue = queue<BookSearchQueue, void>(async (task, callback) => {
+  await searchBook(task.row, task.stream)
+  callback()
+}, 1)
 
-writeStream.pipe(createWriteStream(bookCSVIsbn))
+writeStream.pipe(createWriteStream(bookUpdatedListFile))
 
 readStream
   .on('error', (error) => console.error(error))
-  .on('data', (data) => searchBook(data, writeStream, searchStatus))
-  .on('end', onFinish)
+  .on('data', readRow)
+  .on('end', (rowCount: number) => readFinished(rowCount, searchQueue, writeStream))
