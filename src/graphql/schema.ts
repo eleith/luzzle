5765547c57b141fe91e23b711a5510da5fc8d2b5
eleith <online+github@eleith.com>
2021-04-02 08:32:34 -0700

    make nexus types generation easier

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,26 +1,57 @@
-import { queryType, makeSchema, objectType } from 'nexus'
-import { nexusPrisma } from 'nexus-plugin-prisma'
+import { GraphQLDateTime } from 'graphql-scalars'
+import { queryType, makeSchema, objectType, stringArg, nonNull, asNexusMethod } from 'nexus'
 import path from 'path'
 
-const Query = queryType({
-  definition(t) {
-    t.string('hello', { resolve: () => 'hello world' })
-  },
-})
+const DateTime = asNexusMethod(GraphQLDateTime, 'date')
 
 const Book = objectType({
   name: 'Book',
   definition(t) {
-    t.model.id()
-    t.model.title()
+    t.nonNull.id('id')
+    t.nonNull.string('title')
+    t.string('subtitle')
+    t.string('author')
+    t.string('id_ol_book')
+    t.string('id_ol_work')
+    t.string('isbn')
+    t.string('coauthors')
+    t.string('description')
+    t.string('keywords')
+    t.int('pages')
+    t.int('year_first_published')
+    t.date('date_updated')
+    t.date('date_added')
+  },
+})
+
+const Query = queryType({
+  definition(t) {
+    t.list.field('books', {
+      type: 'Book',
+      resolve: (_parent, _args, ctx) => {
+        return ctx.prisma.book.findMany({})
+      },
+    })
+    t.field('book', {
+      type: 'Book',
+      args: {
+        id: nonNull(stringArg()),
+      },
+      resolve: (_parent, args, ctx) => {
+        return ctx.prisma.book.findUnique({ where: { id: args.id } })
+      },
+    })
   },
 })
 
 export const schema = makeSchema({
-  types: [Query, Book],
-  plugins: [nexusPrisma({ experimentalCRUD: true })],
+  types: [Query, Book, DateTime],
   outputs: {
     typegen: path.join(process.cwd(), 'generated', 'nexus-typegen.ts'),
     schema: path.join(process.cwd(), 'generated', 'schema.graphql'),
+  },
+  contextType: {
+    module: path.join(process.cwd(), 'src', 'graphql', 'context.ts'),
+    export: 'Context',
   },
 })
