c045550b0cc9ba795f9267bef48a14b6686c94f0
eleith <eleith@noreply.git.eleith.com>
2021-10-13 17:13:24 -0700

    develop upload solution

---
+++ tools/lib/web.ts
@@ -0,0 +1,22 @@
+import { createWriteStream } from 'fs'
+import got from 'got'
+import { fromStream } from 'file-type'
+// import tempy from 'tempy'
+
+async function downloadTo(url: string): Promise<string> {
+  const { default: tempy } = await import('tempy')
+
+  const response = got.stream(url)
+  const fileType = await fromStream(response)
+  const filePath = tempy.file({ extension: fileType?.ext || '.unknown' })
+  const writer = createWriteStream(filePath)
+
+  response.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => resolve(filePath))
+    writer.on('error', reject)
+  })
+}
+
+export { downloadTo }
