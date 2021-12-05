a7e32ca6869d7c40e7fab2a651395177b15c2e26
eleith <online+github@eleith.com>
2021-12-04 17:28:11 -0800

    new 3d book component

--- src/common/components/books/index.tsx
+++ src/common/components/books/index.tsx
@@ -1,79 +1,207 @@
 import { ThemeUIStyleObject } from 'theme-ui'
-import { Image, Box } from 'theme-ui'
+import { keyframes } from '@emotion/react'
+import { Box } from 'theme-ui'
 
-export function getCoverPath(slug: string): string {
-  return `/images/covers/${slug}.jpg`
-}
+function getBookContainerStyles(
+  width: number,
+  height: number,
+  thickness: number,
+  perspective: number,
+  rotate: number,
+  rotateHover: number,
+  backgroundColor: string,
+  textColor: string,
+  shadowColor: string,
+  radius: number,
+  pagesOffset: number,
+  transitionDuration: number,
+  backgroundImageUrl?: string
+): ThemeUIStyleObject {
+  /* note: css var do not inherit by psueodo elements */
+  const bookCssVariables: ThemeUIStyleObject = {
+    '--book-width': `${width}px`,
+    '--book-height': `${height}px`,
+    '--book-thickness': `${thickness}px`,
+    '--book-perspective': `${perspective}px`,
+    '--book-rotate': `${rotate}deg`,
+    '--book-rotate-hover': `${rotateHover}deg`,
+    '--book-color': `${backgroundColor}`,
+    '--book-text-color': `${textColor}`,
+    '--book-border-radius': `${radius}px`,
+    '--book-shadow-color': `${shadowColor}`,
+    '--book-page-offset': `${pagesOffset}px`,
+    '--book-transition-duration': `${transitionDuration}s`,
+    '--book-page-height': 'calc(var(--book-height) - 2 * var(--book-page-offset))',
+    '--book-translate': `${width - thickness / 2 - pagesOffset}px`,
+    '--book-transform-thickness': `${-thickness / 2}px`,
+  }
 
-const BookSizes = {
-  SMALL: 1,
-  MEDIUM: 2,
-  LARGE: 3,
-  EXTRA_LARGE: 4,
-} as const
+  const bookContainerStyles: ThemeUIStyleObject = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    perspective: 'var(--book-perspective)',
+  }
 
-type BookSize = typeof BookSizes[keyof typeof BookSizes]
+  const bookRotateAnimation = keyframes({
+    '0%': { transform: `rotateY(${rotateHover}deg)` },
+    '100%': { transform: `rotateY(${rotate}deg)` },
+  })
 
-export function getBookStyle(size: BookSize): ThemeUIStyleObject {
+  const bookStyles: ThemeUIStyleObject = {
+    width: 'var(--book-width)',
+    height: 'var(--book-height)',
+    position: 'relative',
+    color: 'var(--book-text-color)',
+    transformStyle: 'preserve-3d',
+    transform: `rotateY(var(--book-rotate))`,
+    transition: `transform var(--book-transition-duration) ease`,
+    animation: `1s ease 0s 1 ${bookRotateAnimation}`,
+    ':hover': {
+      transform: `rotateY(var(--book-rotate-hover))`,
+    },
+  }
+
+  const bookChildStyles: ThemeUIStyleObject = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: 'var(--book-width)',
+    height: 'var(--book-height)',
+    transform: `translateZ(calc(var(--book-thickness) / 2))`,
+    backgroundColor: 'var(--book-color)',
+    ...(backgroundImageUrl && { backgroundImage: `url('${backgroundImageUrl}')` }),
+    backgroundSize: 'cover',
+    borderTopLeftRadius: '0',
+    borderTopRightRadius: 'var(--book-border-radius)',
+    borderBottomRightRadius: 'var(--book-border-radius)',
+    borderBottomLeftRadius: '0',
+    boxShadow: `5px 5px 20px var(--book-shadow-color)`,
+  }
+
+  const bookBeforeStyles: ThemeUIStyleObject = {
+    position: 'absolute',
+    left: 0,
+    top: 'var(--book-page-offset)',
+    width: 'calc(var(--book-thickness) - 2px)',
+    height: 'var(--book-page-height)',
+    transform: 'translateX(var(--book-translate)) rotateY(90deg)',
+    background: `linear-gradient(90deg, 
+        #fff 0%,
+        #f9f9f9 5%,
+        #fff 10%,
+        #f9f9f9 15%,
+        #fff 20%,
+        #f9f9f9 25%,
+        #fff 30%,
+        #f9f9f9 35%,
+        #fff 40%,
+        #f9f9f9 45%,
+        #fff 50%,
+        #f9f9f9 55%,
+        #fff 60%,
+        #f9f9f9 65%,
+        #fff 70%,
+        #f9f9f9 75%,
+        #fff 80%,
+        #f9f9f9 85%,
+        #fff 90%,
+        #f9f9f9 95%,
+        #fff 100%
+        )`,
+  }
+
+  const bookAfterStyles: ThemeUIStyleObject = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 'var(--book-width)',
+    height: 'var(--book-height)',
+    transform: 'translateZ(var(--book-transform-thickness))',
+    backgroundColor: 'var(--book-color)',
+    borderTopLeftRadius: '0',
+    borderTopRightRadius: 'var(--book-border-radius)',
+    borderBottomRightRadius: 'var(--book-border-radius)',
+    borderBottomLeftRadius: '0',
+    boxShadow: '-10px 0 50px 10px var(--book-shadow-color)',
+  }
+
   return {
-    width: `${size * 10 + 170}px`,
-    height: `${((size * 10 + 170) * 3) / 2}px`,
-    marginLeft: '25px',
-    marginRight: '25px',
-    background: '#FFF',
-    borderLeft: `${size * 5 + 5}px solid #303030`,
-    transform: 'rotate(-15deg) skewX(10deg) scale(.8)',
-    boxShadow: '-50px 50px 50px rgba(0, 0, 0, 0.5)',
-    opacity: '55%',
-    '::before': {
-      content: '""',
-      width: `${size * 5 + 5}px`,
-      height: '100%',
-      background: '#000000',
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      transform: `skewY(-45deg) translate(-${size * 10 + 10}px, -${size * 8 + 7}px)`,
-    },
-    '::after': {
-      content: '""',
-      width: '196px',
-      height: `${size * 5 + 5}px`,
-      background: '#CCC',
-      position: 'absolute',
-      bottom: '0',
-      left: '0',
-      transform: `skewX(-45deg) translate(-${size * 2 + 3}px, ${size * 5 + 5}px)`,
+    ...bookCssVariables,
+    ...bookContainerStyles,
+    '& > :nth-child(1)': {
+      ...bookStyles,
+      '& > :nth-child(1)': {
+        ...bookBeforeStyles,
+      },
+      '& > :nth-child(2)': {
+        ...bookChildStyles,
+      },
+      '& > :nth-child(3)': {
+        ...bookAfterStyles,
+      },
     },
   }
 }
 
-export function getBookSize(pages: number): BookSize {
-  if (pages) {
-    if (pages > 900) {
-      return BookSizes.MEDIUM
-    } else if (pages > 600) {
-      return BookSizes.LARGE
-    } else if (pages > 300) {
-      return BookSizes.EXTRA_LARGE
-    }
-  }
-  return BookSizes.SMALL
+type BookProps = {
+  children: JSX.Element
+  rotateHover?: number
+  rotate?: number
+  perspective?: number
+  transitionDuration?: number
+  radius?: number
+  thickness?: number
+  backgroundColor?: string
+  textColor?: string
+  shadowColor?: string
+  width?: number
+  height?: number
+  pagesOffset?: number
+  backgroundImageUrl?: string
 }
 
-export function makeBook(size: BookSize, coverUrl?: string): JSX.Element {
-  const coverImage = coverUrl && (
-    <Image
-      alt="book cover art for x"
-      src={coverUrl}
-      sx={{
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover',
-        objectPosition: 'left top',
-      }}
-    />
+function BookCover({
+  children,
+  backgroundImageUrl,
+  rotateHover = -5,
+  rotate = -30,
+  perspective = 600,
+  transitionDuration = 1,
+  thickness = 50,
+  backgroundColor = 'black',
+  textColor = 'white',
+  shadowColor = '#aaaaaa',
+  radius = 2,
+  width = 200,
+  height = 300,
+  pagesOffset = 3,
+}: BookProps): JSX.Element {
+  const coverStyles = getBookContainerStyles(
+    width,
+    height,
+    thickness,
+    perspective,
+    rotate,
+    rotateHover,
+    backgroundColor,
+    textColor,
+    shadowColor,
+    radius,
+    pagesOffset,
+    transitionDuration,
+    backgroundImageUrl
   )
-  const bookStyle = getBookStyle(size)
-  return <Box sx={bookStyle}>{coverImage}</Box>
+
+  return (
+    <Box sx={coverStyles}>
+      <Box>
+        <div />
+        <div>{children}</div>
+        <div />
+      </Box>
+    </Box>
+  )
 }
+
+export default BookCover
