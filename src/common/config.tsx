3b40decdcc03a3d8131d39b5b9534a4ff6fca90b
eleith <online+github@eleith.com>
2022-02-14 06:56:05 -0800

    expose env to client

--- src/common/config.tsx
+++ src/common/config.tsx
@@ -1,7 +1,7 @@
 const config = {
   GRAPHQL_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
   HOST: process.env.HOST,
-  HOST_PUBLIC: process.env.HOST_PUBLIC,
+  HOST_PUBLIC: process.env.NEXT_PUBLIC_HOST_STATIC,
 }
 
 export default config
