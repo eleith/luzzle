17be62c02a9bb8b07806c354e5435f4776c91389
eleith <eleith@noreply.git.eleith.com>
2022-02-14 17:32:23 -0800

    get build, submit and deploy working again

--- src/common/hooks/useGraphSWR.tsx
+++ src/common/hooks/useGraphSWR.tsx
@@ -1,14 +1,13 @@
 import { request } from 'graphql-request'
 import useSWR, { SWRConfiguration, SWRResponse } from 'swr'
-import { TypedDocumentNode, ResultOf } from '@graphql-typed-document-node/core'
+import { ResultOf, VariablesOf } from '@graphql-typed-document-node/core'
 import config from '@app/common/config'
+import { DocumentNode } from 'graphql'
 
-type Variables = { [key: string]: string }
-
-export default function useGraphSWR<
-  X extends TypedDocumentNode,
-  V extends Variables,
-  E extends Error
->(gql: X, variables?: V, options?: SWRConfiguration<ResultOf<X>, E>): SWRResponse<ResultOf<X>, E> {
+export default function useGraphSWR<X extends DocumentNode, E extends Error>(
+  gql: X,
+  variables?: VariablesOf<X>,
+  options?: SWRConfiguration<ResultOf<X>, E>
+): SWRResponse<ResultOf<X>, E> {
   return useSWR<ResultOf<X>, E>([config.GRAPHQL_ENDPOINT, gql, variables], request, options)
 }
