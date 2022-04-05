9b56caf6c70de58c3d070ee91634a3e120fae4ba
eleith <online-gitea@eleith.com>
2022-04-04 17:18:35 -0700

    extend limit to 500 until we figure out pagination

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
-  experimental: {
-    esmExternals: 'loose',
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('_http_common')
-      config.externals.push('encoding');
+      config.externals.push('encoding')
     }
     return config
   },
