3abb71c98de9271cd897095d3a8266f5340486cc
eleith <eleith@noreply.git.eleith.com>
2022-04-27 21:04:12 -0700

    add book size/color variations

--- src/common/components/books/BookCoverFor.tsx
+++ src/common/components/books/BookCoverFor.tsx
@@ -2,32 +2,103 @@ import config from '@app/common/config'
 import Link from 'next/link'
 import BookCover, { BookCoverProps } from './BookCover'
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
+import { createHash } from 'crypto'
+
+const BookCoverSize = {
+  SMALL: 'SMALL',
+  MEDIUM: 'MEDIUM',
+  LARGE: 'LARGE',
+} as const
 
 type Book = {
   slug: string
   title: string
   id: string
+  pages?: number | null
 }
 
 type BookCoverForProps = {
   asLink?: boolean
   book: Book
   hasCover?: boolean
+  size?: typeof BookCoverSize
+  scale?: number
 } & Omit<BookCoverProps, 'children' | 'backgroundImageUrl'>
+
+const BookColors = [
+  '$red10',
+  '$purple10',
+  '$indigo10',
+  '$blue10',
+  '$cyan10',
+  '$orange10',
+  '$brown10',
+]
+
+const sizes = {
+  SMALL: {
+    width: 200 * 0.9,
+    height: 300 * 0.9,
+    thickness: 50 * 0.9,
+  },
+  MEDIUM: {
+    width: 200,
+    height: 300,
+    thickness: 50,
+  },
+  LARGE: {
+    width: 200 * 1.1,
+    height: 300 * 1.1,
+    thickness: 50 * 1.1,
+  },
+}
+
+function getColor(slug: string): typeof BookColors[number] {
+  const numColors = BookColors.length
+  const hex = createHash('sha256').update(slug).digest('hex')
+  const random = parseInt(hex, 16) % numColors
+
+  return BookColors[random]
+}
+
+function getSize(pages?: number | null, scale = 1): typeof sizes[keyof typeof BookCoverSize] {
+  let size = sizes.SMALL
+  if (pages) {
+    if (pages >= 700) {
+      size = sizes.LARGE
+    } else if (pages && pages >= 320) {
+      size = sizes.MEDIUM
+    }
+  }
+  return {
+    width: size.width * scale,
+    height: size.height * scale,
+    thickness: size.thickness * scale,
+  }
+}
 
 function BookCoverFor({
   book,
   asLink = false,
   hasCover = false,
+  scale = 1,
   ...coverProps
 }: BookCoverForProps): JSX.Element {
   const coverUrl = `${config.HOST_STATIC}/images/covers/${book.slug}.jpg`
+  const size = getSize(book.pages, scale)
+  const color = getColor(book.slug)
+  const bookCoverProps = {
+    ...size,
+    backgroundColor: color,
+    ...coverProps,
+    backgroundImageUrl: hasCover ? coverUrl : undefined,
+  } as BookCoverProps
 
   if (asLink) {
     return (
       <Link href={`/books/${book.slug}`} key={book.id}>
         <a>
-          <BookCover {...coverProps} backgroundImageUrl={hasCover ? coverUrl : undefined}>
+          <BookCover {...bookCoverProps}>
             <VisuallyHidden>{book.title}</VisuallyHidden>
           </BookCover>
         </a>
@@ -35,7 +106,7 @@ function BookCoverFor({
     )
   } else {
     return (
-      <BookCover {...coverProps} backgroundImageUrl={hasCover ? coverUrl : undefined}>
+      <BookCover {...bookCoverProps}>
         <VisuallyHidden>{book.title}</VisuallyHidden>
       </BookCover>
     )
