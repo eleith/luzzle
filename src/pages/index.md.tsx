dc5954ae202bee12a3cb9b280323d781f8ab85ef
eleith <online-gitea@eleith.com>
2022-05-06 22:51:23 -0700

    ux for seeing all page types

--- src/pages/index.md.tsx
+++ src/pages/index.md.tsx
@@ -9,6 +9,11 @@ import Index from '@app/pages-md/index.mdx'
 import { Box } from '@app/common/components/ui'
 import { jsx2md } from '@app/lib/markdown'
 import components from '@app/common/components/mdx/components'
+import { FileArrowDown, FileArrowUp, FileCode } from 'phosphor-react'
+import { useState } from 'react'
+import { Paragraph } from '@app/common/components/ui/Paragraph'
+import { Flex } from '@app/common/components/ui/Flex'
+import Link from 'next/link'
 
 const getBooksQuery = gql<typeof GetBookHomeMdDocument>(
   `query GetBookHomeMd($take: Int) {
@@ -43,32 +48,65 @@ export async function getStaticProps(): Promise<{ props: HomePageProps }> {
   }
 }
 
-export default function HomeMd({ book1, book2 }: HomePageProps): JSX.Element {
+export default function HomeMd({ book1, book2, markdown }: HomePageProps): JSX.Element {
   const homePage = <Index components={components} book1={book1} book2={book2} />
+  const [view, setView] = useState<'html' | 'md'>('html')
+
+  const html = (
+    <Box
+      css={{
+        width: '100%',
+        margin: 'auto',
+        marginTop: '20px',
+        '@tablet': {
+          width: '100%',
+          margin: 'auto',
+          marginTop: '50px',
+        },
+        '@desktop': {
+          width: '70%',
+          margin: 'auto',
+          marginTop: '50px',
+        },
+      }}
+    >
+      {view === 'html' ? homePage : markdown}
+    </Box>
+  )
+  const md = (
+    <Paragraph as={'code'} size={'1'} css={{ whiteSpace: 'pre-wrap' }}>
+      {markdown}
+    </Paragraph>
+  )
 
   return (
     <Page meta={{ title: '' }}>
-      <Container size={2}>
-        <Box
-          css={{
-            width: '100%',
-            margin: 'auto',
-            marginTop: '20px',
-            '@tablet': {
-              width: '100%',
-              margin: 'auto',
-              marginTop: '50px',
-            },
-            '@desktop': {
-              width: '70%',
-              margin: 'auto',
-              marginTop: '50px',
-            },
-          }}
-        >
-          {homePage}
+      <Flex justify={'end'}>
+        <Link href="/">
+          <a>
+            <FileArrowUp size={50} color={'#6f6f6f'} />
+          </a>
+        </Link>
+        <Box css={{ cursor: view === 'html' ? 'default' : 'pointer' }}>
+          <FileArrowDown
+            size={50}
+            color={view === 'html' ? '#cfcfcf' : '#6f6f6f'}
+            onClick={() => {
+              setView('html')
+            }}
+          />
         </Box>
-      </Container>
+        <Box css={{ cursor: view === 'html' ? 'pointer' : 'default' }}>
+          <FileCode
+            size={50}
+            color={view === 'html' ? '#1f1f1f' : '#cfcfcf'}
+            onClick={() => {
+              setView('md')
+            }}
+          />
+        </Box>
+      </Flex>
+      <Container size={2}>{view === 'html' ? html : md}</Container>
     </Page>
   )
 }
