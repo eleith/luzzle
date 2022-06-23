0f129b2bf61991e793f3ac41bd433149aca876b2
eleith <eleith@noreply.git.eleith.com>
2022-06-23 14:53:13 -0700

    remove empty classnames, adjust book page

--- src/pages/books/[slug].css.ts
+++ src/pages/books/[slug].css.ts
@@ -1,22 +1,24 @@
 import { style } from '@vanilla-extract/css'
-import { vars } from '@app/common/ui/css'
+import { mediaBreakpointLaptop, vars } from '@app/common/ui/css'
 
 export const bookContainer = style({
   display: 'flex',
   width: vars.space.full,
   margin: vars.space.auto,
-  marginTop: vars.space[10],
+  marginTop: vars.space[5],
   gap: vars.space[5],
   justifyContent: 'space-between',
   alignItems: 'center',
-  paddingLeft: vars.space[10],
-  paddingRight: vars.space[10],
 })
 
 export const bookDetails = style({
-  width: vars.space[96],
   lineHeight: vars.lineHeights[1.5],
   marginTop: vars.space[5],
+  "@media": {
+    [mediaBreakpointLaptop]: {
+      width: vars.space['1/3'],
+    },
+  },
 })
 
 export const book = style({
