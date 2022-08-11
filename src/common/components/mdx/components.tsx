7c302950230c2d26af69bb4ad662de7d11c38327
eleith <eleith@noreply.git.eleith.com>
2022-08-11 09:22:44 -0700

    address lint errors

--- src/common/components/mdx/components.tsx
+++ src/common/components/mdx/components.tsx
@@ -4,26 +4,10 @@ import Link from 'next/link'
 
 const components: MDXComponents = {
   h1: (props) => <Text as={'h1'}>{props.children}</Text>,
-  h2: (props) => (
-    <Text as={'h2'}>
-      {props.children}
-    </Text>
-  ),
-  h3: (props) => (
-    <Text as={'h3'}>
-      {props.children}
-    </Text>
-  ),
-  h4: (props) => (
-    <Text as={'h4'}>
-      {props.children}
-    </Text>
-  ),
-  h5: (props) => (
-    <Text as={'h5'}>
-      {props.children}
-    </Text>
-  ),
+  h2: (props) => <Text as={'h2'}>{props.children}</Text>,
+  h3: (props) => <Text as={'h3'}>{props.children}</Text>,
+  h4: (props) => <Text as={'h4'}>{props.children}</Text>,
+  h5: (props) => <Text as={'h5'}>{props.children}</Text>,
   p: (props) => <Text>{props.children}</Text>,
   a: (props) => {
     if (props.href) {
