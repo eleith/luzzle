bae94465e6e43542ee0b7bfeddabe142abaf7a8c
eleith <online-gitea@eleith.com>
2022-08-24 22:03:14 -0700

    [CI Skip] mutation supports error fragments

---
+++ src/common/graphql/error/objects/error.ts
@@ -0,0 +1,12 @@
+import builder from '@app/lib/graphql/builder'
+
+export const ErrorInterface = builder.interfaceRef<Error>('Error').implement({
+  fields: (t) => ({
+    message: t.exposeString('message'),
+  }),
+})
+
+builder.objectType(Error, {
+  name: 'BaseError',
+  interfaces: [ErrorInterface],
+})
