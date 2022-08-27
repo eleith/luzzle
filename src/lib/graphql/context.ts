22c573eafb3c0dac4f048dfa61d22a0520ed0c33
eleith <online-gitea@eleith.com>
2022-08-26 21:59:46 -0700

    wire up email

--- src/lib/graphql/context.ts
+++ src/lib/graphql/context.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@app/prisma'
+import { client } from '@app/common/emailjs'
+import { SMTPClient } from 'emailjs'
 
 const prisma = new PrismaClient()
 
 export interface Context {
   prisma: PrismaClient
+  email: SMTPClient
 }
 
 export function createContext(): Context {
-  return { prisma }
+  return { prisma, email: client }
 }
