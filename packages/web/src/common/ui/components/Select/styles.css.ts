24549fcf9f94642b86db9ad6a4d41e304e81eefe
eleith <eleith@noreply.git.eleith.com>
2022-11-13 07:07:37 -0800

    make components more responsive

--- packages/web/src/common/ui/components/Select/styles.css.ts
+++ packages/web/src/common/ui/components/Select/styles.css.ts
@@ -1,6 +1,6 @@
 import { style } from '@vanilla-extract/css'
 import { RecipeVariants, recipe } from '@vanilla-extract/recipes'
-import { vars } from '../../css'
+import { mediaBreakpointTablet, vars } from '../../css'
 
 export const variants = recipe({
   base: {
@@ -8,7 +8,6 @@ export const variants = recipe({
     fontWeight: vars.fontWeights.normal,
     letterSpacing: '-0.02',
     backgroundColor: vars.colors.surfaceVariant,
-    height: '56px',
     borderTopLeftRadius: vars.radii.small,
     borderTopRightRadius: vars.radii.small,
     borderBottomLeftRadius: '0px',
@@ -20,6 +19,11 @@ export const variants = recipe({
     borderBottomStyle: 'solid',
     borderBottomWidth: '1px',
     borderBottomColor: vars.colors.onSurfaceVariant,
+    '@media': {
+      [mediaBreakpointTablet]: {
+        fontSize: vars.fontSizes.body,
+      },
+    },
     selectors: {
       '&:focus-within': {
         borderBottomColor: vars.colors.primary,
@@ -46,17 +50,22 @@ export const select = style({
   color: vars.colors.onSurfaceVariant,
   width: '100%',
   minWidth: '0px',
-  height: '28px',
   borderRadius: '0px',
   appearance: 'none',
   flexGrow: '1',
-  padding: '0 16px',
+  padding: '0px 8px 4px 8px',
   fontFamily: vars.fonts.sans,
   direction: 'inherit',
   textAlign: 'inherit',
   cursor: 'pointer',
   display: 'flex',
   justifyContent: 'space-between',
+  '@media': {
+    [mediaBreakpointTablet]: {
+      fontSize: vars.fontSizes.body,
+      padding: '0 16px 8px 16px',
+    },
+  },
 })
 
 export const label = style({
@@ -64,8 +73,8 @@ export const label = style({
   fontWeight: vars.fontWeights.light,
   letterSpacing: '-0.02',
   paddingTop: '3px',
-  paddingLeft: '16px',
-  paddingRight: '16px',
+  paddingLeft: '8px',
+  paddingRight: '8px',
   lineHeight: vars.lineHeights['1.5'],
   textAlign: 'left',
   textOverflow: 'ellipsis',
@@ -74,6 +83,13 @@ export const label = style({
   selectors: {
     '[data-invalid=true] &': {
       color: vars.colors.error,
+    },
+  },
+  '@media': {
+    [mediaBreakpointTablet]: {
+      fontSize: vars.fontSizes.label,
+      paddingLeft: '16px',
+      paddingRight: '16px',
     },
   },
 })
@@ -108,15 +124,23 @@ export const selectList = style({
 export const selectItem = style({
   outline: 'none',
   display: 'flex',
+  fontSize: vars.fontSizes.bodyMobile,
+
   scrollMargin: vars.space['0.5'],
   alignItems: 'center',
   gap: vars.space['0.5'],
-  padding: '16px',
+  padding: '8px',
   cursor: 'pointer',
   selectors: {
     '&[data-active-item]': {
       backgroundColor: vars.colors.primary,
       color: vars.colors.onPrimary,
+    },
+  },
+  '@media': {
+    [mediaBreakpointTablet]: {
+      fontSize: vars.fontSizes.body,
+      padding: '16px',
     },
   },
 })
@@ -133,6 +157,11 @@ export const description = style({
   paddingTop: vars.space[1],
   paddingBottom: vars.space[1],
   paddingLeft: vars.space[1],
+  '@media': {
+    [mediaBreakpointTablet]: {
+      fontSize: vars.fontSizes.label,
+    },
+  },
 })
 
 export const error = style({
@@ -141,6 +170,11 @@ export const error = style({
   paddingTop: vars.space[1],
   paddingBottom: vars.space[1],
   paddingLeft: vars.space[1],
+  '@media': {
+    [mediaBreakpointTablet]: {
+      fontSize: vars.fontSizes.label,
+    },
+  },
 })
 
 export const selected = style({
