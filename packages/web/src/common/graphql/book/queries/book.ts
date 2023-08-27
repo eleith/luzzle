942cff277c0c4647d243e5a2a6f5001ce7bc5e8f
eleith <eleith@noreply.git.eleith.com>
2023-08-27 12:18:57 -0700

    linting fixes for convertng to tabs

--- packages/web/src/common/graphql/book/queries/book.ts
+++ packages/web/src/common/graphql/book/queries/book.ts
@@ -2,44 +2,44 @@ import builder from '@app/lib/graphql/builder'
 import BookObject from '../objects/book'
 
 builder.queryFields((t) => ({
-  book: t.field({
-    errors: {
-      types: [Error],
-    },
-    type: BookObject,
-    args: {
-      id: t.arg({ type: 'String' }),
-      slug: t.arg({ type: 'String' }),
-    },
-    resolve: async (_, args, ctx) => {
-      const { slug, id } = args
+	book: t.field({
+		errors: {
+			types: [Error],
+		},
+		type: BookObject,
+		args: {
+			id: t.arg({ type: 'String' }),
+			slug: t.arg({ type: 'String' }),
+		},
+		resolve: async (_, args, ctx) => {
+			const { slug, id } = args
 
-      if (slug) {
-        return await ctx.db
-          .selectFrom('books')
-          .selectAll()
-          .where('slug', '=', slug)
-          .executeTakeFirstOrThrow()
-      } else if (id) {
-        return await ctx.db
-          .selectFrom('books')
-          .selectAll()
-          .where('id', '=', id)
-          .executeTakeFirstOrThrow()
-      } else {
-        const find = await ctx.db
-          .selectFrom('books')
-          .select(ctx.db.fn.count<number>('id').as('book_count'))
-          .executeTakeFirstOrThrow()
-        const skip = Math.floor(Math.random() * find.book_count)
-        const book = await ctx.db
-          .selectFrom('books')
-          .selectAll()
-          .limit(1)
-          .offset(skip)
-          .executeTakeFirstOrThrow()
-        return book
-      }
-    },
-  }),
+			if (slug) {
+				return await ctx.db
+					.selectFrom('books')
+					.selectAll()
+					.where('slug', '=', slug)
+					.executeTakeFirstOrThrow()
+			} else if (id) {
+				return await ctx.db
+					.selectFrom('books')
+					.selectAll()
+					.where('id', '=', id)
+					.executeTakeFirstOrThrow()
+			} else {
+				const find = await ctx.db
+					.selectFrom('books')
+					.select(ctx.db.fn.count<number>('id').as('book_count'))
+					.executeTakeFirstOrThrow()
+				const skip = Math.floor(Math.random() * find.book_count)
+				const book = await ctx.db
+					.selectFrom('books')
+					.selectAll()
+					.limit(1)
+					.offset(skip)
+					.executeTakeFirstOrThrow()
+				return book
+			}
+		},
+	}),
 }))
--- packages/web/src/pages/books/index.tsx
+++ packages/web/src/pages/books/index.tsx
@@ -14,122 +14,122 @@ import { generate } from '@app/common/rss'
 import config from '@app/common/config'
 
 const getBooksQuery = gql<typeof GetBooksDocument>(
-  `query GetBooks($take: Int, $after: String) {
+	`query GetBooks($take: Int, $after: String) {
   books(take: $take, after: $after) {
     ...BookFullDetails
   }
 }
 `,
-  bookFragment
+	bookFragment
 )
 
 type GetBooksQuery = ResultOf<typeof getBooksQuery>
 type Book = NonNullable<GetBooksQuery['books']>[number]
 
 type BooksProps = {
-  books: Book[]
+	books: Book[]
 }
 
 const TAKE = 50
 
 function makeBookCardLink(book: Book): JSX.Element {
-  return (
-    <Link href={`/books/${book.slug}`} key={book.id}>
-      <a target="_blank">
-        <Box>
-          <BookCoverFor
-            book={book}
-            hasCover={!!book.coverWidth}
-            scale={0.5}
-            rotateInteract={{ x: 0, y: -35 }}
-          />
-        </Box>
-      </a>
-    </Link>
-  )
+	return (
+		<Link href={`/books/${book.slug}`} key={book.id}>
+			<a target="_blank">
+				<Box>
+					<BookCoverFor
+						book={book}
+						hasCover={!!book.coverWidth}
+						scale={0.5}
+						rotateInteract={{ x: 0, y: -35 }}
+					/>
+				</Box>
+			</a>
+		</Link>
+	)
 }
 
 export async function getStaticProps(): Promise<{ props: BooksProps }> {
-  const response = await staticClient.query({ query: getBooksQuery, variables: { take: TAKE } })
-  const books = response.data?.books
+	const response = await staticClient.query({ query: getBooksQuery, variables: { take: TAKE } })
+	const books = response.data?.books
 
-  const booksFeed = books?.map((book) => {
-    return {
-      title: book.title,
-      author: [{ name: book.author }],
-      link: `${config.public.HOST}/books/${book.slug}`,
-      image: book.coverWidth
-        ? `${config.public.HOST_STATIC}/images/covers/${book.slug}.jpg`
-        : undefined,
-      description: book.description || '',
-      content: book.description || '',
-      date: new Date(`${book.yearRead || 2000}-${book.monthRead || 1}`),
-    }
-  })
+	const booksFeed = books?.map((book) => {
+		return {
+			title: book.title,
+			author: [{ name: book.author }],
+			link: `${config.public.HOST}/books/${book.slug}`,
+			image: book.coverWidth
+				? `${config.public.HOST_STATIC}/images/covers/${book.slug}.jpg`
+				: undefined,
+			description: book.description || '',
+			content: book.description || '',
+			date: new Date(`${book.yearRead || 2000}-${book.monthRead || 1}`),
+		}
+	})
 
-  generate(booksFeed || [], 'books')
+	generate(booksFeed || [], 'books')
 
-  return {
-    props: {
-      books: books || [],
-    },
-  }
+	return {
+		props: {
+			books: books || [],
+		},
+	}
 }
 
 export default function Books({ books }: BooksProps): JSX.Element {
-  const totalBooks: Book[] = []
-  const [shouldFetch, setFetch] = useState(false)
-  const { data, size, setSize } = useGraphSWRInfinite(
-    (_, previousData: GetBooksQuery | null) => {
-      const lastData = previousData?.books || books
-      if (shouldFetch && lastData.length === TAKE) {
-        const lastBook = lastData?.[TAKE - 1]
-        return {
-          gql: getBooksQuery,
-          variables: { take: TAKE, after: lastBook.readOrder },
-        }
-      } else {
-        return null
-      }
-    },
-    {
-      revalidateFirstPage: false,
-      revalidateOnFocus: false,
-    }
-  )
+	const totalBooks: Book[] = []
+	const [shouldFetch, setFetch] = useState(false)
+	const { data, size, setSize } = useGraphSWRInfinite(
+		(_, previousData: GetBooksQuery | null) => {
+			const lastData = previousData?.books || books
+			if (shouldFetch && lastData.length === TAKE) {
+				const lastBook = lastData?.[TAKE - 1]
+				return {
+					gql: getBooksQuery,
+					variables: { take: TAKE, after: lastBook.readOrder },
+				}
+			} else {
+				return null
+			}
+		},
+		{
+			revalidateFirstPage: false,
+			revalidateOnFocus: false,
+		}
+	)
 
-  // large tables will perform poorly
-  // consider react-virtual when window support lands in v3
+	// large tables will perform poorly
+	// consider react-virtual when window support lands in v3
 
-  const lastPage = data?.[data.length - 1]?.books || []
-  const isEnd = size !== 1 && (lastPage.length === 0 || lastPage.length < TAKE)
+	const lastPage = data?.[data.length - 1]?.books || []
+	const isEnd = size !== 1 && (lastPage.length === 0 || lastPage.length < TAKE)
 
-  if (data) {
-    const pages = data.reduce((total, query) => [...total, ...(query.books || [])], [] as Book[])
-    totalBooks.push(...books, ...pages)
-  } else {
-    totalBooks.push(...books)
-  }
+	if (data) {
+		const pages = data.reduce((total, query) => [...total, ...(query.books || [])], [] as Book[])
+		totalBooks.push(...books, ...pages)
+	} else {
+		totalBooks.push(...books)
+	}
 
-  function loadMore(): void {
-    if (size === 1) {
-      setFetch(true)
-    }
-    setSize(size + 1)
-  }
+	function loadMore(): void {
+		if (size === 1) {
+			setFetch(true)
+		}
+		setSize(size + 1)
+	}
 
-  const allBooks = totalBooks.map((book) => makeBookCardLink(book))
+	const allBooks = totalBooks.map((book) => makeBookCardLink(book))
 
-  return (
-    <PageFull meta={{ title: 'books' }}>
-      <Box>
-        <Box className={styles.booksContainer}>{allBooks}</Box>
-        {!isEnd && (
-          <Box className={styles.booksActions}>
-            <Anchor onClick={loadMore}>get more books</Anchor>
-          </Box>
-        )}
-      </Box>
-    </PageFull>
-  )
+	return (
+		<PageFull meta={{ title: 'books' }}>
+			<Box>
+				<Box className={styles.booksContainer}>{allBooks}</Box>
+				{!isEnd && (
+					<Box className={styles.booksActions}>
+						<Anchor onClick={loadMore}>get more books</Anchor>
+					</Box>
+				)}
+			</Box>
+		</PageFull>
+	)
 }
