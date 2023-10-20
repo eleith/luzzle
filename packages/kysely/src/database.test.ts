4d8df51754e8f2d49c985e4aa1c6cbb2626c505a
eleith <eleith@noreply.git.eleith.com>
2023-10-20 10:48:19 -0700

    linting

--- packages/kysely/src/database.test.ts
+++ packages/kysely/src/database.test.ts
@@ -7,36 +7,36 @@ vi.mock('better-sqlite3')
 vi.mock('kysely')
 
 const mocks = {
-  SqliteDatabase: vi.mocked(SqliteDatabase),
-  Kysely: vi.mocked(Kysely),
-  SqliteDialect: vi.mocked(SqliteDialect),
+	SqliteDatabase: vi.mocked(SqliteDatabase),
+	Kysely: vi.mocked(Kysely),
+	SqliteDialect: vi.mocked(SqliteDialect),
 }
 
 describe('database', () => {
-  afterEach(() => {
-    vi.resetAllMocks()
-  })
+	afterEach(() => {
+		vi.resetAllMocks()
+	})
 
-  test('getDatabaseClient', async () => {
-    const path = 'path/to/db.sqlite'
+	test('getDatabaseClient', async () => {
+		const path = 'path/to/db.sqlite'
 
-    getDatabaseClient(path)
+		getDatabaseClient(path)
 
-    expect(mocks.SqliteDatabase).toHaveBeenCalledWith(path)
-    expect(mocks.SqliteDialect).toHaveBeenCalledOnce()
-    expect(mocks.Kysely).toHaveBeenCalledOnce()
-  })
+		expect(mocks.SqliteDatabase).toHaveBeenCalledWith(path)
+		expect(mocks.SqliteDialect).toHaveBeenCalledOnce()
+		expect(mocks.Kysely).toHaveBeenCalledOnce()
+	})
 
-  test('getDatabaseClient debug', async () => {
-    const path = 'path/to/db.sqlite'
+	test('getDatabaseClient debug', async () => {
+		const path = 'path/to/db.sqlite'
 
-    getDatabaseClient(path, true)
+		getDatabaseClient(path, true)
 
-    expect(mocks.SqliteDatabase).toHaveBeenCalledWith(path)
-    expect(mocks.SqliteDialect).toHaveBeenCalledOnce()
-    expect(mocks.Kysely).toHaveBeenCalledWith({
-      log: ['query', 'error'],
-      dialect: expect.anything(),
-    })
-  })
+		expect(mocks.SqliteDatabase).toHaveBeenCalledWith(path)
+		expect(mocks.SqliteDialect).toHaveBeenCalledOnce()
+		expect(mocks.Kysely).toHaveBeenCalledWith({
+			log: ['query', 'error'],
+			dialect: expect.anything(),
+		})
+	})
 })
