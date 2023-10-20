343757c2a4dfe68cbb3412bd464469662dfb55e3
eleith <eleith@noreply.git.eleith.com>
2023-10-20 09:31:57 -0700

    exports to support piece abstraction
    to support piece abstraction, we need better types and type names
    
    also adopted style conventions used in cli package
    
    there is a new table being experimented with, but that is on hold and
    commented out. this exercise surfaced the need for type updates

--- packages/kysely/src/database.schema.ts
+++ packages/kysely/src/database.schema.ts
@@ -4,51 +4,84 @@ type cuid = string
 type date_added = ColumnType<number, undefined, never>
 type date_updated = ColumnType<number | null, undefined, number>
 
+/*
+export interface LinksTable {
+	id: cuid
+	title: string
+	subtitle: string | null
+	author: string | null
+	coauthors: string | null
+	summary: string | null
+	year_read: number | null
+	month_read: number | null
+	date_published: number | null
+	date_added: date_added
+	date_updated: date_updated
+	keywords: string | null
+	slug: string
+	note: string | null
+	read_order: string
+	archive_path: string | null
+	url: string | null
+	archive_url: string | null
+	cover_width: number | null
+	cover_height: number | null
+	cover_path: string | null
+}
+*/
 export interface BooksTable {
-  id: cuid
-  id_ol_book: string | null
-  id_ol_work: string | null
-  isbn: string | null
-  title: string
-  subtitle: string | null
-  author: string
-  coauthors: string | null
-  description: string | null
-  pages: number | null
-  year_read: number | null
-  month_read: number | null
-  year_first_published: number | null
-  date_added: date_added
-  date_updated: date_updated
-  keywords: string | null
-  cover_width: number | null
-  cover_height: number | null
-  cover_path: string | null
-  slug: string
-  note: string | null
-  read_order: string
+	id: cuid
+	id_ol_book: string | null
+	id_ol_work: string | null
+	isbn: string | null
+	title: string
+	subtitle: string | null
+	author: string
+	coauthors: string | null
+	description: string | null
+	pages: number | null
+	year_read: number | null
+	month_read: number | null
+	year_first_published: number | null
+	date_added: date_added
+	date_updated: date_updated
+	keywords: string | null
+	cover_width: number | null
+	cover_height: number | null
+	cover_path: string | null
+	slug: string
+	note: string | null
+	read_order: string
 }
 
 export interface TagMapsTable {
-  id_tag: cuid
-  id_item: cuid
-  type: 'books'
-  date_added: date_added
-  date_updated: date_updated
+	id_tag: cuid
+	id_item: cuid
+	type: 'books'
+	date_added: date_added
+	date_updated: date_updated
 }
 
 export interface TagsTable {
-  id: cuid
-  slug: string
-  name: string
-  date_added: date_added
-  date_updated: date_updated
+	id: cuid
+	slug: string
+	name: string
+	date_added: date_added
+	date_updated: date_updated
+}
+
+export const PieceTable = {
+	Books: 'books',
+} as const
+
+export interface PieceDatabase {
+	[PieceTable.Books]: BooksTable
 }
 
 export interface Database {
-  books: BooksTable
-  tag_maps: TagMapsTable
-  tags: TagsTable
+	tag_maps: TagMapsTable
+	tags: TagsTable
+	[PieceTable.Books]: BooksTable
 }
 
 export type LuzzleDatabase = Kysely<Database>
@@ -64,3 +97,10 @@ export type TagUpdate = Updateable<TagsTable>
 export type TagMap = Selectable<TagMapsTable>
 export type TagMapInsert = Insertable<TagMapsTable>
 export type TagMapUpdate = Updateable<TagMapsTable>
+
+// export type Link = Selectable<LinksTable>
+// export type LinkInsert = Insertable<LinksTable>
+// export type LinkUpdate = Updateable<LinksTable>
+
+export type PieceTables = keyof PieceDatabase & string
+export type Pieces = PieceDatabase[PieceTables]
