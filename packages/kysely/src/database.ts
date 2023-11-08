97c75e57b902ed48bba0f2a9c8f53b7fa9e1c6fe
eleith <eleith@noreply.git.eleith.com>
2023-11-08 06:06:39 -0800

    add links table
    re-arrange and rename many exports
    
    abstract out piece tables so we can reference books and links together
    or independently
    
    bump version

--- packages/kysely/src/database.ts
+++ packages/kysely/src/database.ts
@@ -1,9 +1,9 @@
 import { Kysely, SqliteDialect } from 'kysely'
 import SqliteDatabase from 'better-sqlite3'
-import { Database } from './database.schema'
+import { LuzzleTables } from './database.schema'
 
 function getDatabaseClient(pathToDb: string, debug = false) {
-	return new Kysely<Database>({
+	return new Kysely<LuzzleTables>({
 		log: debug ? ['query', 'error'] : [],
 		dialect: new SqliteDialect({
 			database: new SqliteDatabase(pathToDb),
