7de19932492835e2884ea06587e687cf26604823
eleith <eleith@noreply.git.eleith.com>
2023-11-21 21:37:20 -0800

    support generalized attachments in book piece

--- packages/cli/src/lib/commands/attach.test.ts
+++ packages/cli/src/lib/commands/attach.test.ts
@@ -6,6 +6,7 @@ import { makeContext } from './context.fixtures.js'
 import { makePieceCommand, parsePieceArgv, downloadFileOrUrlTo } from '../pieces/index.js'
 import { makeMarkdownSample, makePiece } from '../pieces/piece.fixtures.js'
 import { unlink } from 'fs/promises'
+import log from '../log.js'
 
 vi.mock('fs/promises')
 vi.mock('../pieces/index')
@@ -18,6 +19,7 @@ const mocks = {
 	getPiece: vi.fn(),
 	download: vi.mocked(downloadFileOrUrlTo),
 	unlink: vi.mocked(unlink),
+	logError: vi.spyOn(log, 'error'),
 }
 
 const spies: { [key: string]: SpyInstance } = {}
@@ -53,11 +55,41 @@ describe('lib/commands/attach', () => {
 		mocks.download.mockResolvedValueOnce(tmpFile)
 		mocks.unlink.mockResolvedValueOnce(undefined)
 		spies.pieceGet = vi.spyOn(PieceTest.prototype, 'get').mockResolvedValue(markdown)
-		spies.pieceAttach = vi.spyOn(PieceTest.prototype, 'attach')
+		spies.pieceAttach = vi.spyOn(PieceTest.prototype, 'attach').mockResolvedValue(markdown)
+		spies.pieceWrite = vi.spyOn(PieceTest.prototype, 'write')
 
 		await command.run(ctx, { path, file, field } as Arguments<AttachArgv>)
 
 		expect(spies.pieceAttach).toHaveBeenCalledWith(tmpFile, markdown, field, undefined)
+		expect(spies.pieceWrite).toHaveBeenCalledWith(markdown)
+		expect(mocks.unlink).toHaveBeenCalledWith(tmpFile)
+	})
+
+	test('run catches error', async () => {
+		const path = 'slug2'
+		const file = 'file2'
+		const tmpFile = 'tmpFile'
+		const field = 'field'
+		const PieceTest = makePiece()
+		const pieceType = 'piece'
+		const markdown = makeMarkdownSample()
+		const ctx = makeContext({
+			pieces: {
+				getPieceTypes: mocks.getPieceTypes.mockReturnValue([pieceType]),
+				getPiece: mocks.getPiece.mockResolvedValue(new PieceTest()),
+			},
+		})
+
+		mocks.piecesParseArgs.mockReturnValueOnce({ piece: 'books', slug: path })
+		mocks.download.mockResolvedValueOnce(tmpFile)
+		mocks.unlink.mockResolvedValueOnce(undefined)
+		spies.pieceGet = vi.spyOn(PieceTest.prototype, 'get').mockResolvedValue(markdown)
+		spies.pieceAttach = vi.spyOn(PieceTest.prototype, 'attach').mockRejectedValue(new Error('test'))
+
+		await command.run(ctx, { path, file, field } as Arguments<AttachArgv>)
+
+		expect(spies.pieceAttach).toHaveBeenCalledWith(tmpFile, markdown, field, undefined)
+		expect(mocks.logError).toHaveBeenCalledOnce()
 	})
 
 	test('run with dry-run', async () => {
