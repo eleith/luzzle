bd54cc3ce100ca5008be10230e11dee1e112f25e
eleith <online-gitea@eleith.com>
2024-01-08 19:29:10 -0800

    run migrations and version bump

--- packages/kysely/src/database.migrations.ts
+++ packages/kysely/src/database.migrations.ts
@@ -4,6 +4,8 @@ import * as migration1 from './migrations/2023-05-26T15:04:19.094Z'
 import * as migration2 from './migrations/2023-10-30T23:24:40Z'
 import * as migration3 from './migrations/2023-11-22T14:55:18Z'
 import * as migration4 from './migrations/2023-11-27T04:32:07Z'
+import * as migration5 from './migrations/2024-01-01T23:30:29Z'
+import * as migration6 from './migrations/2024-01-06T15:52:43Z'
 
 class LuzzleMigrationProvider implements MigrationProvider {
 	async getMigrations() {
@@ -12,6 +14,8 @@ class LuzzleMigrationProvider implements MigrationProvider {
 			'2': migration2,
 			'3': migration3,
 			'4': migration4,
+			'5': migration5,
+			'6': migration6,
 		}
 	}
 }
