d744a27290834776206bdec56e67ad908112f6f9
eleith <online-gitea@eleith.com>
2024-01-08 19:25:30 -0800

    sync command filter fix

--- packages/cli/src/lib/commands/sync.ts
+++ packages/cli/src/lib/commands/sync.ts
@@ -40,7 +40,7 @@ const command: Command<SyncArgv> = {
 				slugs.push(optionalPiece.slug)
 			} else {
 				const allSlugs = await pieces.getSlugs()
-				const updatedSlugs = await pieces.filterSlugsBy(allSlugs, 'lastProcessed')
+				const updatedSlugs = await pieces.filterSlugsBy(allSlugs, 'lastSynced')
 				const processSlugs = force ? allSlugs : updatedSlugs
 
 				slugs.push(...processSlugs)
