ea173aefdc74bdc4b79cb8bb6d66a191c4a35282
eleith <eleith@noreply.git.eleith.com>
2024-02-09 14:30:37 -0800

    [kysely] add pieces table view
    in order to be able to query all pieces for general metadata, we need a
    common table

--- packages/kysely/src/database.migrations.ts
+++ packages/kysely/src/database.migrations.ts
@@ -6,16 +6,18 @@ import * as migration3 from './migrations/2023-11-22T14:55:18Z'
 import * as migration4 from './migrations/2023-11-27T04:32:07Z'
 import * as migration5 from './migrations/2024-01-01T23:30:29Z'
 import * as migration6 from './migrations/2024-01-06T15:52:43Z'
+import * as migration7 from './migrations/2024-02-09T16:33:10Z'
 
 class LuzzleMigrationProvider implements MigrationProvider {
 	async getMigrations() {
 		return {
-			'1': migration1,
-			'2': migration2,
-			'3': migration3,
-			'4': migration4,
-			'5': migration5,
-			'6': migration6,
+			1: migration1,
+			2: migration2,
+			3: migration3,
+			4: migration4,
+			5: migration5,
+			6: migration6,
+			7: migration7,
 		}
 	}
 }
---
+++ packages/kysely/src/migrations/2024-02-09T16:33:10Z.ts
@@ -0,0 +1,41 @@
+import { Kysely } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+	const booksQuery = db
+		.selectFrom('books')
+		.select('id')
+		.select('note')
+		.select('slug')
+		.select('date_added')
+		.select('date_updated')
+		.select('title')
+		.select('keywords')
+		.select('cover as media')
+		.select('date_read as date_order')
+		.select("'books' as from_piece")
+
+	const linksQuery = db
+		.selectFrom('links')
+		.select('id')
+		.select('note')
+		.select('slug')
+		.select('date_added')
+		.select('date_updated')
+		.select('title')
+		.select('keywords')
+		.select('representative_image as media')
+		.select('date_accessed as date_order')
+		.select("'links' as from_piece")
+
+	const unionedQuery = booksQuery.unionAll(linksQuery)
+
+	// add view for all pieces
+	await db.schema.createView('pieces_view').as(unionedQuery).execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+	// remove the view
+	await db.schema.dropView('pieces_view').execute()
+}
