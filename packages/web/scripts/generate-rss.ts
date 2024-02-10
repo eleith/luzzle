b5947cb5513f0045a113fec00e955624411921a0
eleith <online-gitea@eleith.com>
2024-02-10 06:46:08 -0800

    [web] rss for pieces and types

--- packages/web/scripts/generate-rss.ts
+++ packages/web/scripts/generate-rss.ts
@@ -1,15 +1,15 @@
 import { Feed, Item } from 'feed'
 import { writeFile, mkdir } from 'fs/promises'
 import { loadEnvConfig } from '@next/env'
-import { getDatabaseClient, PieceSelectable } from '@luzzle/kysely'
+import { getDatabaseClient, LuzzleSelectable } from '@luzzle/kysely'
 
 loadEnvConfig(process.cwd(), process.env.NODE_ENV !== 'production')
 
-async function generateRss(books: PieceSelectable<'books'>[], type: string) {
+async function generateRss(pieces: LuzzleSelectable<'pieces_view'>[], folder: string) {
 	const feed = new Feed({
-		title: process.env.TITLE || '',
-		description: process.env.DESCRIPTION,
-		id: `${process.env.NEXT_PUBLIC_HOST}/${type}`,
+		title: process.env.SITE_TITLE || '',
+		description: process.env.SITE_DESCRIPTION,
+		id: `${process.env.NEXT_PUBLIC_HOST}/${folder}`,
 		link: process.env.NEXT_PUBLIC_HOST,
 		ttl: 60 * 24,
 		// image,
@@ -17,10 +17,10 @@ async function generateRss(books: PieceSelectable<'books'>[], type: string) {
 		updated: new Date(),
 		generator: 'jpmonette/feed',
 		language: 'en',
-		copyright: `© ${new Date().getFullYear()}`,
+		copyright: `🄯 ${new Date().getUTCFullYear()}`,
 		feedLinks: {
-			rss2: `${process.env.NEXT_PUBLIC_HOST}/rss/${type}/feed.xml`,
-			json: `${process.env.NEXT_PUBLIC_HOST}/rss/${type}/feed.json`,
+			rss2: `${process.env.NEXT_PUBLIC_HOST}/rss/${folder}/feed.xml`,
+			json: `${process.env.NEXT_PUBLIC_HOST}/rss/${folder}/feed.json`,
 		},
 		// author: {
 		//   name: '',
@@ -29,29 +29,28 @@ async function generateRss(books: PieceSelectable<'books'>[], type: string) {
 		// },
 	})
 
-	const items = books?.map((book) => {
-		return {
-			title: book.title,
-			author: [{ name: book.author }],
-			link: `${process.env.NEXT_PUBLIC_HOST}/books/${book.slug}`,
-			image: book.cover
-				? `${process.env.NEXT_PUBLIC_HOST_STATIC}/images/og/books/${book.slug}.png`
-				: undefined,
-			description: book.description || '',
-			content: book.description || '',
-			date: new Date(book.date_read ?? book.date_added),
-		} as Item
+	const items = pieces?.map((piece) => {
+		const item: Item = {
+			title: piece.title,
+			link: `${process.env.NEXT_PUBLIC_HOST}/${piece.from_piece}/${piece.slug}`,
+			image: `${process.env.NEXT_PUBLIC_HOST_STATIC}/images/og/${piece.from_piece}/${piece.slug}.png`,
+			description: piece.note || '',
+			content: piece.note || '',
+			date: new Date(piece.date_order ?? piece.date_added),
+		}
+
+		return item
 	})
 
 	items.forEach((item) => {
 		feed.addItem(item)
 	})
 
-	await mkdir(`./public/rss/${type}`, { recursive: true })
+	await mkdir(`./public/rss/${folder}`, { recursive: true })
 
 	// https://github.com/jpmonette/feed/issues/140
 	await writeFile(
-		`./public/rss/${type}/feed.xml`,
+		`./public/rss/${folder}/feed.xml`,
 		feed
 			.rss2()
 			.replace(
@@ -64,13 +63,33 @@ async function generateRss(books: PieceSelectable<'books'>[], type: string) {
 async function main() {
 	const db = getDatabaseClient(`${process.env.LUZZLE_FOLDER}/luzzle.sqlite`)
 	const books = await db
-		.selectFrom('books')
+		.selectFrom('pieces_view')
 		.limit(50)
-		.orderBy('date_added', 'desc')
+		.where('from_piece', '=', 'books')
+		.orderBy('date_order', 'desc')
 		.selectAll()
 		.execute()
 
-	generateRss(books, 'books')
+	generateRss(books, 'pieces/books')
+
+	const links = await db
+		.selectFrom('pieces_view')
+		.limit(50)
+		.where('from_piece', '=', 'links')
+		.orderBy('date_order', 'desc')
+		.selectAll()
+		.execute()
+
+	generateRss(links, 'pieces/links')
+
+	const pieces = await db
+		.selectFrom('pieces_view')
+		.limit(50)
+		.orderBy('date_order', 'desc')
+		.selectAll()
+		.execute()
+
+	generateRss(pieces, 'pieces')
 }
 
 main()
