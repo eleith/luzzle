96e77ec3ea478311be78639d53f3642038b0edfc
eleith <eleith@noreply.git.eleith.com>
2024-02-20 06:25:12 -0800

    [web] remove fuse and lodash dependencies

--- packages/web/src/lib/graphql/tag.ts
+++ packages/web/src/lib/graphql/tag.ts
@@ -1,13 +1,12 @@
 import gqlTag from 'graphql-tag'
 import { DocumentNode } from 'graphql'
-import { isArray } from 'lodash'
 
 function gql<T extends DocumentNode>(
 	operation: string,
 	fragments?: DocumentNode | DocumentNode[]
 ): T {
 	if (fragments) {
-		if (isArray(fragments)) {
+		if (fragments.constructor === Array) {
 			const fragment = fragments.reduce((all, f) => gqlTag`${all}${f}`)
 			return gqlTag`${operation}${fragment}` as T
 		} else {
