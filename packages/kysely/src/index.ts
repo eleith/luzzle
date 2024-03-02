ead07de70784e96ea2d89ef62ce56d36b645f56f
eleith <online-gitea@eleith.com>
2024-03-01 19:42:32 -0800

    [kysely] add cache table
    1. table migrations
    2. fix trigger bugs
    3. migrating schema parsing logic into this package

--- packages/kysely/src/index.ts
+++ packages/kysely/src/index.ts
@@ -3,7 +3,7 @@ import { getDatabaseClient } from './database.js'
 import migrate from './database.migrations'
 
 export * from './database.schema.js'
-export * from './tables/pieces.js'
+export * from './tables/pieces.schema.js'
 export * from './jtd/index.js'
 
 export { getDatabaseClient, migrate, sql }
