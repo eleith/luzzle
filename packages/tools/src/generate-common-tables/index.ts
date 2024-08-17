0331642c626343a92aa32dcd1901d36a500ef85d
eleith <online-gitea@eleith.com>
2024-08-17 05:23:49 -0700

    [tools] rebuild fts5 table
    on an external backing, a rebuild is needed to trigger the initialize
    index, otherwise search won't work even though there is content in the
    search table

--- packages/tools/src/generate-common-tables/index.ts
+++ packages/tools/src/generate-common-tables/index.ts
@@ -21,10 +21,6 @@ async function createWebTables(db: LuzzleDatabase): Promise<void> {
 		.addColumn('date_consumed', 'datetime')
 		.execute()
 
-	await sql`CREATE VIRTUAL TABLE IF NOT EXISTS "web_pieces_fts5" USING fts5(id UNINDEXED, slug, type UNINDEXED, title, summary, note, media UNINDEXED, keywords, json_metadata, date_added UNINDEXED, date_updated UNINDEXED, date_consumed UNINDEXED, tokenize = 'porter ascii', prefix='3 4 5', content = 'web_pieces_common', content_rowid="rowid")`.execute(
-		db
-	)
-
 	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'books' as type, title as title, description as summary, note as note, cover as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_read as date_consumed, json_object('url', url, 'isbn', isbn, 'year_first_published', year_first_published, 'subtitle', subtitle, 'author', author, 'coauthors', coauthors, 'pages', pages) as json_metadata FROM pieces_items_books`.execute(
 		db
 	)
@@ -40,11 +36,21 @@ async function createWebTables(db: LuzzleDatabase): Promise<void> {
 	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'texts' as type, title as title, summary as summary, note as note, representative_image as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_published as date_consumed, json_object('subtitle', subtitle, 'attachments', attachments) as json_metadata FROM pieces_items_texts`.execute(
 		db
 	)
+
+	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'films' as type, title as title, summary as summary, note as note, poster as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_viewed as date_consumed, json_object('subtitle', subtitle, 'language', language, 'runtime', runtime, 'date_released', date_released, 'backdrop', backdrop, 'homepage', homepage, 'url', url, 'type', type) as json_metadata FROM pieces_items_films`.execute(
+		db
+	)
+
+	await sql`CREATE VIRTUAL TABLE IF NOT EXISTS "web_pieces_fts5" USING fts5(id UNINDEXED, slug, type UNINDEXED, title, summary, note, media UNINDEXED, keywords, json_metadata, date_added UNINDEXED, date_updated UNINDEXED, date_consumed UNINDEXED, tokenize = 'porter ascii', prefix='3 4 5', content = 'web_pieces', content_rowid="rowid")`.execute(
+		db
+	)
+
+	await sql`INSERT INTO web_pieces_fts5(web_pieces_fts5) VALUES('rebuild')`.execute(db)
 }
 
 async function dropWebTables(db: LuzzleDatabase): Promise<void> {
-	await db.schema.dropTable('web_pieces').execute()
-	await db.schema.dropTable('web_pieces_fts5').execute()
+	await db.schema.dropTable('web_pieces').ifExists().execute()
+	await db.schema.dropTable('web_pieces_fts5').ifExists().execute()
 }
 
 async function run(): Promise<void> {
