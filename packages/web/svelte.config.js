dcd02d02229e79eda4b6abade3d65f5e8ff932c1
eleith <eleith@noreply.git.eleith.com>
2024-10-07 10:20:53 -0700

    [web] turn off CDN for paths
    we currently build twice, once in docker and once outside so we can
    upload to CDN, but we are seeing variations in the filenames
    
    turning it off for now to get a working deploy. will revisit

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,12 +1,6 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp'
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import dotenv from 'dotenv'
-
-dotenv.config({ path: '.env.local', override: true })
-
-const production = process.env.NODE_ENV === 'production'
-const cdn = process.env.PUBLIC_ASSETS_CDN_URL || ''
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
@@ -21,9 +15,6 @@ const config = {
 		prerender: {
 			concurrency: 4,
 			handleHttpError: 'warn'
-		},
-		paths: {
-			assets: production ? cdn : ''
 		}
 	}
 }
