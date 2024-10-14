4121f1c983c13c8b7ef3a7ccd624c60c8bc2ecce
eleith <eleith@noreply.git.eleith.com>
2024-10-13 22:06:41 -0700

    [web] breakout luzzle pre-build scripts

--- packages/web/tools/luzzle/images.ts
+++ packages/web/tools/luzzle/images.ts
@@ -1,3 +1,5 @@
+import { generateVariantsForPieces } from './variants'
+import { generateOpenGraphsForPieces } from './opengraph'
 import { initialize } from './database'
 
 import dotenv from 'dotenv'
@@ -19,8 +21,8 @@ async function prebuildLuzzle() {
 
 	console.log(pieces.length, PRIVATE_DATABASE_URL)
 
-	//await generateVariantsForPieces(pieces)
-	//await generateOpenGraphsForPieces(pieces)
+	await generateVariantsForPieces(pieces)
+	await generateOpenGraphsForPieces(pieces)
 
 	console.log('[done] prebuilding luzzle')
 }
