d589d041077f74140179391e5899d6a23399257b
eleith <online-gitea@eleith.com>
2024-10-20 17:14:11 -0700

    [web] remove theme cookie, use local storage and injected body script

--- packages/web/src/hooks.ts
+++ packages/web/src/hooks.ts
@@ -1,20 +1,9 @@
 import type { Handle, Reroute } from '@sveltejs/kit'
-import themes, { type Theme } from '$lib/ui/styles/themes'
 import { sequence } from '@sveltejs/kit/hooks'
 
-export const handleThemeCookie: Handle = async ({ event, resolve }) => {
-	const theme = event.cookies.get('theme') as Theme | undefined | 'system'
-
-	if (!theme || !themes.includes(theme as Theme) || theme === 'system') {
-		return await resolve(event)
-	}
-
-	return await resolve(event, {
-		transformPageChunk: ({ html }) => {
-			return html.replace('data-theme="system"', `data-theme="${theme}"`)
-		}
-	})
+export const handler: Handle = async ({ event, resolve }) => {
+	return await resolve(event)
 }
 
-export const handle: Handle = sequence(handleThemeCookie)
+export const handle: Handle = sequence(handler)
 export const reroute: Reroute = () => {}
