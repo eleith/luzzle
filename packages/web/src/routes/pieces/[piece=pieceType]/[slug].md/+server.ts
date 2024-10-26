0fce8967fece5fc8c711c1708ef6d5b5cd6d614f
eleith <online-gitea@eleith.com>
2024-10-26 15:14:45 -0700

    [web] clean md output

--- packages/web/src/routes/pieces/[piece=pieceType]/[slug].md/+server.ts
+++ packages/web/src/routes/pieces/[piece=pieceType]/[slug].md/+server.ts
@@ -3,31 +3,32 @@ import { getDatabase } from '$lib/database'
 import { addFrontMatter } from '@luzzle/core'
 
 export const GET: RequestHandler = async (a) => {
-	const type = a.params.piece
-	const slug = a.params.slug
 	const db = getDatabase()
 	const contentType = 'text/markdown'
 
 	const piece = await db
 		.selectFrom('web_pieces')
 		.selectAll()
-		.where('type', '=', type)
-		.where('slug', '=', slug)
+		.where('type', '=', a.params.piece)
+		.where('slug', '=', a.params.slug)
 		.executeTakeFirst()
 
 	if (!piece) {
-		return new Response('piece not found', { headers: { 'content-type': contentType }, status: 404 })
+		return new Response('piece not found', {
+			headers: { 'content-type': contentType },
+			status: 404
+		})
 	}
 
 	const metadata = JSON.parse(piece.json_metadata)
+	const nonMetadata = ['slug', 'id', 'note', 'date_added', 'date_updated']
+	const metadataValid = Object.fromEntries(
+		Object.entries(metadata).filter(
+			([key, value]) => (value !== null || value === '') && !nonMetadata.includes(key)
+		)
+	)
 
-	delete metadata.slug
-	delete metadata.id
-	delete metadata.note
-	delete metadata.date_added
-	delete metadata.date_updated
-
-	const body = addFrontMatter(piece.note, metadata)
+	const body = addFrontMatter(piece.note, metadataValid)
 
 	return new Response(body, { headers: { 'content-type': contentType } })
 }
