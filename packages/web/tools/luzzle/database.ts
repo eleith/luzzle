f3106937ee72a2a13f35e7e8c05ac9ba800f2ebd
eleith <eleith@noreply.git.eleith.com>
2024-10-25 21:17:57 -0700

    [web] add md format for pieces

--- packages/web/tools/luzzle/database.ts
+++ packages/web/tools/luzzle/database.ts
@@ -19,30 +19,136 @@ async function createWebTables(db: LuzzleDatabase): Promise<void> {
 		.addColumn('date_consumed', 'datetime')
 		.execute()
 
-	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'books' as type, title as title, description as summary, note as note, cover as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_read as date_consumed, json_object('url', url, 'isbn', isbn, 'year_first_published', year_first_published, 'subtitle', subtitle, 'author', author, 'coauthors', coauthors, 'pages', pages) as json_metadata FROM pieces_items_books`.execute(
-		db
-	)
+	const tables = await db.introspection.getTables()
+	const webColumns = [
+		'id',
+		'slug',
+		'title',
+		'type',
+		'summary',
+		'note',
+		'media',
+		'keywords',
+		'date_added',
+		'date_updated',
+		'date_consumed',
+		'json_metadata'
+	] as Array<keyof WebPieces>
 
-	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'games' as type, title as title, description as summary, note as note, representative_image as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_played as date_consumed, json_object('url', url, 'publisher', publisher, 'date_published', date_published, 'play_time', play_time, 'played_on', played_on, 'play_time', play_time, 'number_of_players', number_of_players, 'type', type) as json_metadata FROM pieces_items_games`.execute(
-		db
-	)
+	const bookTable = 'pieces_items_books'
+	const books = tables.find((table) => table.name === bookTable)
+	const bookColumns = books?.columns.map((column) => column.name) || []
+	const bookColumnsJsonInsert = bookColumns.map((column) => `'${column}', ${column}`).join(', ')
+	const bookColumnsWebInsert = [
+		'id',
+		'slug',
+		'title',
+		"'books'",
+		'description',
+		'note',
+		'cover',
+		'keywords',
+		'date_added',
+		'date_updated',
+		'date_read',
+		'json_object(' + bookColumnsJsonInsert + ')'
+	].join(', ')
 
-	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'links' as type, title as title, summary as summary, note as note, representative_image as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_accessed as date_consumed, json_object('url', url, 'is_active', is_active, 'is_paywall', is_paywall, 'type', type, 'author', author, 'subtitle', subtitle, 'coauthors', coauthors, 'archive_path', archive_path, 'word_count', word_count) as json_metadata FROM pieces_items_links`.execute(
-		db
-	)
+	const gameTable = 'pieces_items_games'
+	const games = tables.find((table) => table.name === gameTable)
+	const gameColumns = games?.columns.map((column) => column.name) || []
+	const gameColumnsJsonInsert = gameColumns.map((column) => `'${column}', ${column}`).join(', ')
+	const gameColumnsWebInsert = [
+		'id',
+		'slug',
+		'title',
+		"'games'",
+		'description',
+		'note',
+		'representative_image',
+		'keywords',
+		'date_added',
+		'date_updated',
+		'date_played',
+		'json_object(' + gameColumnsJsonInsert + ')'
+	].join(', ')
 
-	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'texts' as type, title as title, summary as summary, note as note, representative_image as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_published as date_consumed, json_object('subtitle', subtitle, 'attachments', attachments) as json_metadata FROM pieces_items_texts`.execute(
-		db
-	)
+	const linkTable = 'pieces_items_links'
+	const links = tables.find((table) => table.name === linkTable)
+	const linkColumns = links?.columns.map((column) => column.name) || []
+	const linkColumnsJsonInsert = linkColumns.map((column) => `'${column}', ${column}`).join(', ')
+	const linkColumnsWebInsert = [
+		'id',
+		'slug',
+		'title',
+		"'links'",
+		'summary',
+		'note',
+		'representative_image',
+		'keywords',
+		'date_added',
+		'date_updated',
+		'date_accessed',
+		'json_object(' + linkColumnsJsonInsert + ')'
+	].join(', ')
 
-	await sql`INSERT INTO web_pieces (id, slug, type, title, summary, note, media, keywords, date_added, date_updated, date_consumed, json_metadata) SELECT id as id, slug as slug, 'films' as type, title as title, summary as summary, note as note, poster as media, keywords as keywords, date_added as date_added, date_updated as date_updated, date_viewed as date_consumed, json_object('subtitle', subtitle, 'language', language, 'runtime', runtime, 'date_released', date_released, 'backdrop', backdrop, 'homepage', homepage, 'url', url, 'type', type, 'people', people) as json_metadata FROM pieces_items_films`.execute(
-		db
-	)
+	const textTable = 'pieces_items_texts'
+	const texts = tables.find((table) => table.name === textTable)
+	const textColumns = texts?.columns.map((column) => column.name) || []
+	const textColumnsJsonInsert = textColumns.map((column) => `'${column}', ${column}`).join(', ')
+	const textColumnsWebInsert = [
+		'id',
+		'slug',
+		'title',
+		"'texts'",
+		'summary',
+		'note',
+		'representative_image',
+		'keywords',
+		'date_added',
+		'date_updated',
+		'date_published',
+		'json_object(' + textColumnsJsonInsert + ')'
+	].join(', ')
+
+	const filmTable = 'pieces_items_films'
+	const films = tables.find((table) => table.name === filmTable)
+	const filmColumns = films?.columns.map((column) => column.name) || []
+	const filmColumnsJsonInsert = filmColumns.map((column) => `'${column}', ${column}`).join(', ')
+	const filmColumnsWebInsert = [
+		'id',
+		'slug',
+		'title',
+		"'films'",
+		'summary',
+		'note',
+		'poster',
+		'keywords',
+		'date_added',
+		'date_updated',
+		'date_viewed',
+		'json_object(' + filmColumnsJsonInsert + ')'
+	].join(', ')
+
+	await sql`INSERT INTO web_pieces (${sql.raw(webColumns.join(', '))}) SELECT ${sql.raw(
+		bookColumnsWebInsert
+	)} FROM ${sql.raw(bookTable)}`.execute(db)
+	await sql`INSERT INTO web_pieces (${sql.raw(webColumns.join(', '))}) SELECT ${sql.raw(
+		gameColumnsWebInsert
+	)} FROM ${sql.raw(gameTable)}`.execute(db)
+	await sql`INSERT INTO web_pieces (${sql.raw(webColumns.join(', '))}) SELECT ${sql.raw(
+		linkColumnsWebInsert
+	)} FROM ${sql.raw(linkTable)}`.execute(db)
+	await sql`INSERT INTO web_pieces (${sql.raw(webColumns.join(', '))}) SELECT ${sql.raw(
+		textColumnsWebInsert
+	)} FROM ${sql.raw(textTable)}`.execute(db)
+	await sql`INSERT INTO web_pieces (${sql.raw(webColumns.join(', '))}) SELECT ${sql.raw(
+		filmColumnsWebInsert
+	)} FROM ${sql.raw(filmTable)}`.execute(db)
 
 	await sql`CREATE VIRTUAL TABLE IF NOT EXISTS "web_pieces_fts5" USING fts5(id UNINDEXED, slug, type UNINDEXED, title, summary, note, media UNINDEXED, keywords, json_metadata, date_added UNINDEXED, date_updated UNINDEXED, date_consumed UNINDEXED, tokenize = 'porter ascii', prefix='3 4 5', content = 'web_pieces', content_rowid="rowid")`.execute(
 		db
 	)
-
 	await sql`INSERT INTO web_pieces_fts5(web_pieces_fts5) VALUES('rebuild')`.execute(db)
 }
 
