51d1bd6013afe20641498c9fe7b5318db770b673
eleith <eleith@noreply.git.eleith.com>
2025-03-09 17:47:02 -0700

    [web] pass env into dotenv command instead

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,9 +1,6 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp'
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import dotenv from '@dotenvx/dotenvx'
-
-dotenv.config({ path: '.env.local' })
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
