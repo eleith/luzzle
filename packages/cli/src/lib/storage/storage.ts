93b1846381c78f2131f289126eed8a134291ca04
eleith <eleith@noreply.git.eleith.com>
2025-03-22 14:30:59 -0700

    [cli] update readable to readable stream types

--- packages/cli/src/lib/storage/storage.ts
+++ packages/cli/src/lib/storage/storage.ts
@@ -1,4 +1,4 @@
-import { Readable, Writable } from 'stream'
+import { ReadStream, WriteStream } from 'fs'
 
 export type StorageType = 'fs' | 'webdav'
 export type StorageStat = {
@@ -12,13 +12,13 @@ abstract class Storage {
 	abstract root: string
 	abstract parseArgPath(path: string): string
 	abstract readFile(path: string, format: 'text' | 'binary'): Promise<string>
-	abstract writeFile(path: string, contents: string | Buffer | Readable): Promise<void>
+	abstract writeFile(path: string, contents: string | Buffer | ReadStream): Promise<void>
 	abstract getFilesIn(path: string, options?: { deep?: boolean }): Promise<string[]>
 	abstract exists(path: string): Promise<boolean>
 	abstract delete(path: string): Promise<void>
 	abstract stat(path: string): Promise<StorageStat>
-	abstract createReadStream(path: string): Readable
-	abstract createWriteStream(path: string): Writable
+	abstract createReadStream(path: string): ReadStream
+	abstract createWriteStream(path: string): WriteStream
 	abstract makeDirectory(path: string): Promise<void>
 }
 
