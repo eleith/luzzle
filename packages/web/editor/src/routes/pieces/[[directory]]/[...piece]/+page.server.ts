17db727aa990b899a01761d644d956ac602563a2
eleith <online-gitea@eleith.com>
2025-03-25 05:48:02 -0700

    [web-editor] save and asset uploading

--- packages/web/editor/src/routes/pieces/[[directory]]/[...piece]/+page.server.ts
+++ packages/web/editor/src/routes/pieces/[[directory]]/[...piece]/+page.server.ts
@@ -1,6 +1,8 @@
 import { error } from '@sveltejs/kit'
-import type { PageServerLoad } from './$types'
+import type { Actions, PageServerLoad } from './$types'
 import { getPieces } from '$lib/storage'
+import { Readable } from 'stream'
+import { ReadableStream } from 'stream/web'
 
 export const load: PageServerLoad = async ({ params }) => {
 	const directory = params.directory || ''
@@ -28,3 +30,55 @@ export const load: PageServerLoad = async ({ params }) => {
 		directory
 	}
 }
+
+export const actions = {
+	default: async (event) => {
+		const directory = event.params.directory || ''
+		const file = `${directory}/${event.params.piece}`
+		const pieces = getPieces()
+		const type = pieces.parseFilename(file).type
+		const data = await event.request.formData()
+
+		if (!type) {
+			return error(404, `piece type does not exist`)
+		}
+
+		const piece = await pieces.getPiece(type)
+		let markdown = await piece.get(file)
+
+		if (!markdown) {
+			return error(404, `piece does not exist`)
+		}
+
+		const note = data.get('note')
+
+		if (note !== markdown.note) {
+			markdown.note = (note as string) || ''
+		}
+
+		for (const field of piece.fields) {
+			const key = field.name
+			if (data.has(`frontmatter.${key}`)) {
+				const value = data.get(`frontmatter.${key}`)
+
+				if (field.format === 'asset') {
+					if (data.has(`upload.${key}`)) {
+						const file = data.get(`upload.${key}`) as File
+						if (file) {
+							const stream = Readable.fromWeb(file.stream() as ReadableStream<Uint8Array>)
+							markdown = await piece.setField(markdown, key, stream)
+						}
+					}
+				} else if (value !== markdown.frontmatter[key]) {
+					if (value !== '') {
+						markdown = await piece.setField(markdown, key, value)
+					} else {
+						markdown = await piece.removeField(markdown, key)
+					}
+				}
+			}
+		}
+
+		await piece.write(markdown)
+	}
+} satisfies Actions
--- packages/web/editor/src/routes/pieces/[[directory]]/[...piece]/+page.svelte
+++ packages/web/editor/src/routes/pieces/[[directory]]/[...piece]/+page.svelte
@@ -1,29 +1,129 @@
 <script lang="ts">
 	let { data } = $props()
+	let mode: 'edit' | 'preview' = $state('preview')
+	let formElement = $state<HTMLFormElement>()
+
+	function submit() {
+		formElement?.submit()
+	}
+
+	function formatDateStringForInput(dateString: string): string | null {
+		if (!dateString) {
+			return null
+		}
+
+		try {
+			const date = new Date(dateString)
+
+			if (isNaN(date.getTime())) {
+				return null // Invalid date
+			}
+
+			const year = date.getUTCFullYear()
+			const month = String(date.getUTCMonth() + 1).padStart(2, '0')
+			const day = String(date.getUTCDate()).padStart(2, '0')
+
+			return `${year}-${month}-${day}`
+		} catch (error) {
+			console.log(error)
+			return null
+		}
+	}
 </script>
 
-<section class="main">
-	<div class="piece-container">
-		<div class="field">folder</div>
-		<div><a href="/directory/{data.directory}">{data.directory}</a></div>
-
-		{#each data.schema as field}
-			<div class="field">{field.name}</div>
-			{#if data.fields[field.name]}
-				{#if field.format === 'asset'}
-					<div><a href="/asset/{data.fields[field.name]}">{data.fields[field.name]}</a></div>
-				{:else}
-					<div>{data.fields[field.name]}</div>
-				{/if}
-			{:else}
-				<div><em>empty</em></div>
-			{/if}
-		{/each}
-	</div>
+<section class="toolbar">
+	{#if mode === 'preview'}
+		<button onclick={() => (mode = 'edit')}>edit</button>
+	{:else if mode === 'edit'}
+		<button onclick={() => (mode = 'preview')}>cancel</button>
+		<button onclick={submit}>save</button>
+	{/if}
+	<div><a href="/directory/{data.directory}">{data.directory}</a></div>
 </section>
 
+{#if mode === 'preview'}
+	<section class="preview">
+		<div class="piece-container">
+			{#each data.schema as field}
+				<div class="field">{field.name}</div>
+				{#if data.fields[field.name]}
+					{#if field.format === 'asset'}
+						<div><a href="/asset/{data.fields[field.name]}">{data.fields[field.name]}</a></div>
+					{:else}
+						<div>{data.fields[field.name]}</div>
+					{/if}
+				{:else}
+					<div><em>empty</em></div>
+				{/if}
+			{/each}
+			<div class="field">note</div>
+			<div>{data.note}</div>
+		</div>
+	</section>
+{:else if mode === 'edit'}
+	<section class="edit">
+		<form method="post" bind:this={formElement} enctype="multipart/form-data">
+			<div class="piece-container">
+				{#each data.schema as field}
+					<div class="field">{field.name}</div>
+					{#if field.format === 'asset'}
+						<div>
+							<input type="file" name="upload.{field.name}" />
+							<input
+								type="hidden"
+								name="frontmatter.{field.name}"
+								value={data.fields[field.name]}
+							/>
+						</div>
+					{:else if field.format === 'date'}
+						<div>
+							<input
+								type="date"
+								name="frontmatter.{field.name}"
+								value={formatDateStringForInput(data.fields[field.name] as string) || ''}
+							/>
+						</div>
+					{:else if field.type === 'integer'}
+						<div>
+							<input
+								type="number"
+								name="frontmatter.{field.name}"
+								value={data.fields[field.name]}
+							/>
+						</div>
+					{:else if field.type === 'boolean'}
+						<div>
+							<select name="frontmatter.{field.name}" value={data.fields[field.name] ? 1 : 0}>
+								<option value="1">true</option>
+								<option value="0">false</option>
+							</select>
+						</div>
+					{:else if field.enum}
+						<div>
+							<select name="frontmatter.{field.name}" value={data.fields[field.name]}>
+								{#each field.enum as option}
+									<option value={option}>{option}</option>
+								{/each}
+							</select>
+						</div>
+					{:else}
+						<div>
+							<input type="text" name="frontmatter.{field.name}" value={data.fields[field.name]} />
+						</div>
+					{/if}
+				{/each}
+				<div class="field">note</div>
+				<div>
+					<textarea name="note">{data.note}</textarea>
+				</div>
+			</div>
+		</form>
+	</section>
+{/if}
+
 <style>
-	section.main {
+	section.edit,
+	section.preview {
 		margin: var(--space-4);
 		margin-bottom: var(--space-8);
 		margin-left: auto;
@@ -34,8 +134,21 @@
 		gap: var(--space-4);
 	}
 
+	section.toolbar {
+		margin: var(--space-4);
+		margin-bottom: var(--space-8);
+		margin-left: auto;
+		margin-right: auto;
+		width: 85%;
+		display: flex;
+		flex-direction: row;
+		gap: var(--space-4);
+	}
+
 	@media screen and (min-width: 768px) {
-		section.main {
+		section.edit,
+		section.preview,
+		section.toolbar {
 			width: clamp(500px, 66.6666%, 1000px);
 		}
 	}
