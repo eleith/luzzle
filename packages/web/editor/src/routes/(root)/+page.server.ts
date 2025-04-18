8f6555299accae3e515b1632b22cd3e49c1ffd64
eleith <eleith@noreply.git.eleith.com>
2025-04-18 13:11:09 -0700

    [web-editor] deploy to release webhook

--- packages/web/editor/src/routes/(root)/+page.server.ts
+++ packages/web/editor/src/routes/(root)/+page.server.ts
@@ -1,5 +1,10 @@
 import { getPieces } from '$lib/pieces'
+import { type Actions, fail } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
+import {
+	PRIVATE_LUZZLE_DEPLOY_WEBHOOK,
+	PRIVATE_LUZZLE_DEPLOY_WEBHOOK_BODY
+} from '$env/static/private'
 
 export const load: PageServerLoad = async () => {
 	const pieces = getPieces()
@@ -14,3 +19,26 @@ export const load: PageServerLoad = async () => {
 		}
 	}
 }
+
+export const actions = {
+	deploy: async () => {
+		try {
+			const date = new Date().getTime()
+			const api = new URL(PRIVATE_LUZZLE_DEPLOY_WEBHOOK)
+
+			const response = await fetch(api, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: PRIVATE_LUZZLE_DEPLOY_WEBHOOK_BODY.replaceAll('%tag%', date.toString())
+			})
+
+			if (response.ok) {
+				return { success: true }
+			}
+
+			throw new Error(`failed deploy: ${response.status} ${response.statusText}`)
+		} catch (e) {
+			return fail(400, { error: { message: `failed deploy: ${e}` } })
+		}
+	}
+} satisfies Actions
