3d21b594c58aa71b467821538e6936caa3aad2da
eleith <online-gitea@eleith.com>
2025-06-25 22:49:40 -0700

    [web-editor] deploy webhook with access token

--- packages/web/editor/src/routes/(root)/+page.server.ts
+++ packages/web/editor/src/routes/(root)/+page.server.ts
@@ -3,7 +3,8 @@ import { type Actions, fail } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 import {
 	PRIVATE_LUZZLE_DEPLOY_WEBHOOK,
-	PRIVATE_LUZZLE_DEPLOY_WEBHOOK_BODY
+	PRIVATE_LUZZLE_DEPLOY_WEBHOOK_BODY,
+	PRIVATE_LUZZLE_DEPLOY_WEBHOOK_ACCESS_TOKEN
 } from '$env/static/private'
 
 export const load: PageServerLoad = async () => {
@@ -23,16 +24,21 @@ export const load: PageServerLoad = async () => {
 export const actions = {
 	deploy: async () => {
 		try {
-			const date = new Date().getTime()
 			const api = new URL(PRIVATE_LUZZLE_DEPLOY_WEBHOOK)
 
 			const response = await fetch(api, {
 				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: PRIVATE_LUZZLE_DEPLOY_WEBHOOK_BODY.replaceAll('%tag%', date.toString())
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${PRIVATE_LUZZLE_DEPLOY_WEBHOOK_ACCESS_TOKEN}`
+				},
+				body: PRIVATE_LUZZLE_DEPLOY_WEBHOOK_BODY
 			})
 
 			if (response.ok) {
+				console.log(response)
+				console.log(await response.text())
+
 				return { success: true }
 			}
 
