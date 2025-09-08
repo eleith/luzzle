ea5579defd8173f291991a9e50c403d2efb1d999
eleith <online-gitea@eleith.com>
2025-09-08 13:10:04 -0700

    [tools] decouple open graph generation
    attempting to improve open graph generation. this is an initial commit
    to explore that goal
    
    1. make generating one open graph a quick command for quite iteration
    2. use puppeteer and screenshotting for better open graph image control
    3. use eta.js for templating so html can be isolated
    4. use fs storage instead of sqlite to be able to directly use
    	 frontmatter metadata

---
+++ packages/tools/src/test-open-graph/utils/toReactElement.ts
@@ -0,0 +1,189 @@
+// MIT License: https://github.com/etherCorps/svelte-h2j?tab=readme-ov-file#license
+// function from https://github.com/etherCorps/svelte-h2j/blob/svelte-v5/src/lib/toReactElement.ts
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { parse } from 'svelte/compiler'
+import { walk } from 'estree-walker'
+import { type AST } from 'svelte/compiler'
+
+function extractStyles(cssAst: NonNullable<AST.Root['css']>) {
+	const styles: Record<string, string> = {}
+	if (Object.hasOwn(cssAst, 'children') && cssAst.children.length) {
+		for (const { prelude, block } of cssAst.children) {
+			if (
+				prelude &&
+				typeof prelude !== 'string' &&
+				prelude.children &&
+				prelude.children.length > 0
+			) {
+				for (const selector of prelude.children) {
+					const classNames = selector.children
+						.filter((node: any) => node.type === 'ClassSelector')
+						.map((node: any) => node.name)
+					for (const className of classNames) {
+						styles[className] =
+							block?.children
+								.map((declaration: Record<string, any>) => {
+									return `${declaration.property}: ${declaration.value}`
+								})
+								.join('; ') + ';'
+					}
+				}
+			}
+		}
+	}
+	return styles
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface VNode {
+	type: string
+	props: {
+		style?: Record<string, any>
+		children?: string | VNode | VNode[]
+		[prop: string]: any
+	}
+	key: string | null
+}
+
+//  type ComponentToJsxOptions = {
+//    props?: Record<string, any>
+//    style?: string // Internal CSS: Svelte has removed css from component render output
+//  }
+
+/* Start of code from satori-html for cssToObject converter*/
+const camelize = (ident: string) => ident.replace(/-([a-z])/g, (_, char) => char.toUpperCase())
+const cssToObject = (str: string) => {
+	const obj: Record<string, string> = {}
+	let t = 0
+	let pair = ['', '']
+	const flags: Record<string, number> = { '(': 0, ')': 0 }
+	for (const c of str) {
+		if (!flags['('] && c === ':') {
+			t = 1
+		} else if (c === ';') {
+			const [decl = '', value = ''] = pair
+			obj[camelize(decl.trim())] = value.trim()
+			t = 0
+			pair = ['', '']
+		} else {
+			pair[t] += c
+			switch (c) {
+				case '(': {
+					flags[c]++
+					break
+				}
+				case ')': {
+					flags['(']--
+					break
+				}
+			}
+		}
+	}
+	const [decl = '', value = ''] = pair
+	if (decl.trim() && value.trim()) {
+		obj[camelize(decl.trim())] = value.trim()
+	}
+
+	return obj
+}
+const nodeMap = new WeakMap()
+
+const root: VNode = {
+	type: 'div',
+	props: {
+		style: {
+			display: 'flex',
+			flexDirection: 'column',
+			width: '100%',
+			height: '100%',
+		},
+		children: [],
+	},
+	key: null,
+}
+/* End of satori-html */
+
+function toReactElement(htmlString: string): VNode {
+	const svelteAST = parse(htmlString)
+	let styles: Record<string, string> = {}
+	if (svelteAST && svelteAST.css) {
+		styles = extractStyles(svelteAST.css as unknown as NonNullable<AST.Root['css']>)
+	}
+
+	walk(svelteAST as any, {
+		enter(node: any, parent: any, _prop: any, index: any) {
+			let newNode: any = {}
+			if (node.type === 'Fragment') {
+				nodeMap.set(node, root)
+			} else if (node.type === 'Element') {
+				newNode.type = node.name
+				let { ...props } = node.attributes
+				if (node.attributes.length > 0) {
+					let classStyles: string | null = null
+					let styleExists = false
+					node.attributes.forEach((attribute: any) => {
+						if (Object.keys(styles).length && attribute.name === 'class') {
+							// Remove classes added by svelte
+							const originalClasses = attribute.value[0].data
+								.split(' ')
+								.filter((className: string) => !className.startsWith('svelte'))
+
+							classStyles = originalClasses.reduce((joinedStyles: string, className: string) => {
+								const classStyle = styles[className]
+								if (classStyle) joinedStyles += classStyle
+								return joinedStyles
+							}, '')
+						}
+
+						if (attribute.name === 'style') {
+							styleExists = true
+							const newRawStyle = classStyles
+								? attribute.value[0].data + '; ' + classStyles
+								: attribute.value[0].data
+							props['style'] = cssToObject(newRawStyle) as any
+						} else {
+							props[attribute.name] = attribute.value[0].data as any
+						}
+					})
+					if (Object.keys(styles).length && !styleExists && classStyles) {
+						props['style'] = cssToObject(classStyles)
+					}
+					delete props[0]
+				}
+				// numbered props failing on svgs
+				props = Object.entries(props).reduce((newProps: any, [key, value]) => {
+					if (Number.isNaN(Number(key))) {
+						newProps[key] = value as any
+					}
+					return newProps
+				}, {})
+				Object.assign(newNode, { props })
+				nodeMap.set(node, newNode)
+				if (parent) {
+					const newParent = nodeMap.get(parent)
+					if (!newParent.props.children) {
+						newParent.props.children = []
+					}
+					newParent.props.children[index] = newNode
+				}
+			} else if (node.type === 'Text') {
+				newNode = node.data.trim()
+				if (newNode) {
+					if (parent && parent.type !== 'Attribute') {
+						const newParent = nodeMap.get(parent)
+						if (parent.children.length === 1) {
+							newParent.props.children = newNode
+						} else {
+							newParent.props.children[index] = newNode
+						}
+					}
+				}
+			}
+		},
+	})
+
+	return root
+}
+
+export default toReactElement
