77249fb8947d0b3e1985fbb38b4bcf80f9fe9559
eleith <eleith@noreply.git.eleith.com>
2025-09-17 09:12:21 -0700

    [tools] helpers for template local image srcs

--- packages/tools/src/generate-open-graph/html.ts
+++ packages/tools/src/generate-open-graph/html.ts
@@ -13,6 +13,12 @@ function bufferToBase64(buffer: Buffer, type: string, format: string) {
 	return `data:${type}/${format};base64,${base64}`
 }
 
+const countWords = (text: string): number => {
+  const withoutHtml = text.replace(/<[^>]*>/g, ' ');
+  const words = withoutHtml.match(/[a-zA-Z0-9]+/g);
+  return words ? words.length : 0;
+}
+
 function getDynamicFontSize(title: string, baseSize: number, lineLength: number) {
 	const decayRate = 0.85
 	const minScale = 0.5
@@ -23,20 +29,31 @@ function getDynamicFontSize(title: string, baseSize: number, lineLength: number)
 	return Math.round(baseSize * finalScale)
 }
 
+function getHashIndexFor(word: string, max: number): number {
+	let hash = 0
+	for (let i = 0; i < word.length; i++) {
+		const char = word.charCodeAt(i)
+		hash = (hash << 5) - hash + char
+		hash = hash & hash // Convert to 32bit integer
+	}
+	return Math.abs(hash) % max
+}
+
 async function fontToBase64(fontPath: string) {
 	const fontBuffer = await readFile(fontPath)
 	const ext = path.extname(fontPath).slice(1)
 	return bufferToBase64(fontBuffer, 'font', ext)
 }
 
-async function extractImage(image: Buffer, resize?: { width?: number; height?: number }) {
-	const width = resize?.width
-	const height = resize?.height
+async function extractImage(image: Buffer, options?: { width?: number; height?: number, format: 'jpeg' | 'png' }) {
+	const width = options?.width
+	const height = options?.height
+	const sharpFormat: 'jpeg' | 'png' = options?.format || 'jpeg'
 	const sharpResize = width || height ? { width, height } : undefined
-	const jpgBuffer = await Sharp(image).clone().resize(sharpResize).toFormat('jpg').toBuffer()
+	const jpgBuffer = await Sharp(image).clone().resize(sharpResize).toFormat(sharpFormat).toBuffer()
 	const palette = await new Vibrant(jpgBuffer).getPalette()
 	return {
-		base64: bufferToBase64(jpgBuffer, 'image', 'jpeg'),
+		base64: bufferToBase64(jpgBuffer, 'image', sharpFormat),
 		palette,
 	}
 }
@@ -52,12 +69,18 @@ async function generateHtml(
 	const size = { width: OpenGraphImageWidth, height: OpenGraphImageHeight }
 	const templateDir = path.dirname(template)
 	const helpers = {
-		getDynamicFontSize,
+		countWords,
+		extractImage: async (image: string, resize?: { width: number; height: number, format: 'jpeg' | 'png' }) => {
+			const buffer = await readFile(path.join(templateDir, image))
+			return extractImage(buffer, resize)
+		},
 		fontToBase64: async (font: string) => {
 			const templatePath = path.join(templateDir, font)
 			return fontToBase64(templatePath)
 		},
-		pieceExtractImage: async (image: string, resize?: { width: number; height: number }) => {
+		getDynamicFontSize,
+		getHashIndexFor,
+		pieceExtractImage: async (image: string, resize?: { width: number; height: number, format: 'jpeg' | 'png' }) => {
 			const buffer = await pieces.getPieceAsset(image)
 			return extractImage(buffer, resize)
 		},
