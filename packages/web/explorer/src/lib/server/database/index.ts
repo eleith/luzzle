777f9ea60de219f5154fc4f4b4046d9ef0a8e144
eleith <online-gitea@eleith.com>
2025-09-23 13:45:10 -0700

    [web-explorer] release 0.71 and fix database build requirement

--- packages/web/explorer/src/lib/server/database/index.ts
+++ packages/web/explorer/src/lib/server/database/index.ts
@@ -1,9 +1,11 @@
+import { building } from '$app/environment'
 import { config } from '$lib/server/config'
 import { getDatabaseClient, sql } from '@luzzle/core'
 import type { WebPieceTags, WebPieces } from '$lib/pieces/types'
 
 function initializeDatabase() {
-	return getDatabaseClient(config.paths.database).withTables<{
+	const dbPath = building ? ':memory:' : config.paths.database
+	return getDatabaseClient(dbPath).withTables<{
 		web_pieces: WebPieces
 		web_pieces_fts5: WebPieces
 		web_pieces_tags: WebPieceTags
