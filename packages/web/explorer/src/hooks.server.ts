67b8c0126c965b950585f9cd301bb21bd19d7b86
eleith <online-gitea@eleith.com>
2025-09-25 20:21:05 -0700

    [web-explorer] lint format

--- packages/web/explorer/src/hooks.server.ts
+++ packages/web/explorer/src/hooks.server.ts
@@ -1,24 +1,24 @@
 import '$lib/server/config'
 import '$lib/server/database'
 import type { Handle } from '@sveltejs/kit'
-import { dev } from '$app/environment';
-import { generateThemeCss, minifyCss } from '$lib/ui/styles/theme.generator';
+import { dev } from '$app/environment'
+import { generateThemeCss, minifyCss } from '$lib/ui/styles/theme.generator'
 
-const rawCss = generateThemeCss();
-const themeCss = dev ? rawCss : minifyCss(rawCss);
+const rawCss = generateThemeCss()
+const themeCss = dev ? rawCss : minifyCss(rawCss)
 
 export const handle: Handle = async ({ event, resolve }) => {
-  let buffer = '';
+	let buffer = ''
 
-  const response = await resolve(event, {
-    transformPageChunk: ({ html, done }) => {
-      buffer += html;
+	const response = await resolve(event, {
+		transformPageChunk: ({ html, done }) => {
+			buffer += html
 
-      if (done) {
-        return buffer.replace('%luzzle.theme.style.tag%', `<style>${themeCss}</style>`);
-      }
-    },
-  });
+			if (done) {
+				return buffer.replace('%luzzle.theme.style.tag%', `<style>${themeCss}</style>`)
+			}
+		}
+	})
 
-  return response;
-};
+	return response
+}
