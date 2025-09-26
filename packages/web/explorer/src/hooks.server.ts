06f372624bc953b98a6c47514721f371047f4c61
eleith <online-gitea@eleith.com>
2025-09-25 20:20:46 -0700

    [web-explorer] use lightningcss for minification

--- packages/web/explorer/src/hooks.server.ts
+++ packages/web/explorer/src/hooks.server.ts
@@ -4,10 +4,7 @@ import type { Handle } from '@sveltejs/kit'
 import { dev } from '$app/environment';
 import { generateThemeCss, minifyCss } from '$lib/ui/styles/theme.generator';
 
-// Generate the CSS string only ONCE when the module is first loaded.
 const rawCss = generateThemeCss();
-
-// Minify the CSS only when in production for better performance.
 const themeCss = dev ? rawCss : minifyCss(rawCss);
 
 export const handle: Handle = async ({ event, resolve }) => {
