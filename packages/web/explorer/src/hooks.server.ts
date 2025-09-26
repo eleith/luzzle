9615280c0dd012523e92f80a85134de475105dab
eleith <eleith@noreply.git.eleith.com>
2025-09-25 20:41:30 -0700

    [web-explorer] dynamic and external css for theme

--- packages/web/explorer/src/hooks.server.ts
+++ packages/web/explorer/src/hooks.server.ts
@@ -1,24 +1,2 @@
 import '$lib/server/config'
 import '$lib/server/database'
-import type { Handle } from '@sveltejs/kit'
-import { dev } from '$app/environment'
-import { generateThemeCss, minifyCss } from '$lib/ui/styles/theme.generator'
-
-const rawCss = generateThemeCss()
-const themeCss = dev ? rawCss : minifyCss(rawCss)
-
-export const handle: Handle = async ({ event, resolve }) => {
-	let buffer = ''
-
-	const response = await resolve(event, {
-		transformPageChunk: ({ html, done }) => {
-			buffer += html
-
-			if (done) {
-				return buffer.replace('%luzzle.theme.style.tag%', `<style>${themeCss}</style>`)
-			}
-		}
-	})
-
-	return response
-}
