83949c51bd5b3e864298b294fdbeb8db432ae781
eleith <online-gitea@eleith.com>
2025-09-28 14:58:57 +0000

    [tools] migrate config scripts into tools (#21)
    Reviewed-on: https://git.eleith.com/eleith/luzzle/pulls/21

---
+++ packages/tools/src/check-config/yargs.ts
@@ -0,0 +1,15 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+export function parseArgs() {
+    return yargs(hideBin(process.argv))
+        .strict()
+        .options({
+            config: {
+                type: 'string',
+                description: 'Path to a custom user config file to check',
+            },
+        })
+        .help()
+        .parseSync();
+}
