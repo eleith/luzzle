e7cbea78c2bd81197ced3f325a61881d781951a7
eleith <eleith@noreply.git.eleith.com>
2025-10-01 19:49:57 -0700

    [tools] fix path settings for variant and opengraph generation

--- packages/tools/src/commands/variants/variants.ts
+++ packages/tools/src/commands/variants/variants.ts
@@ -1,30 +1,42 @@
 import Sharp from 'sharp'
 import { Pieces } from '@luzzle/cli'
+import { LuzzleSelectable } from '@luzzle/core'
 
-function generateVariantSharpJob(sharp: Sharp.Sharp, width: number, format: 'avif' | 'jpg') {
-	return sharp.clone().resize({ width }).toFormat(format)
+function generateVariantSharpJob(sharp: Sharp.Sharp, width?: number, format?: 'avif' | 'jpg') {
+	if (width && format) {
+		return sharp.clone().resize({ width }).toFormat(format)
+	} else {
+		return sharp.clone()
+	}
 }
 
 async function generateVariantJobs(
+	item: LuzzleSelectable<'pieces_items'>,
 	asset: string,
 	pieces: Pieces,
 	sizes: Array<number>,
 	formats: Array<'avif' | 'jpg'>
 ) {
 	const jobs: {
-		size: (typeof sizes)[number]
-		format: (typeof formats)[number]
+		size?: (typeof sizes)[number]
+		format?: (typeof formats)[number]
 		sharp: Sharp.Sharp
 	}[] = []
 
-	const image = await pieces.getPieceAsset(asset)
-	const sharpImage = Sharp(image)
+	try {
+		const image = await pieces.getPieceAsset(asset)
+		const sharpImage = Sharp(image)
 
-	for (const format of formats) {
-		for (const size of sizes) {
-			const sharp = generateVariantSharpJob(sharpImage, size, format)
-			jobs.push({ size, format, sharp })
+		for (const format of formats) {
+			for (const size of sizes) {
+				const sharp = generateVariantSharpJob(sharpImage, size, format)
+				jobs.push({ size, format, sharp })
+			}
 		}
+
+		jobs.push({ sharp: sharpImage })
+	} catch (error) {
+		console.error(`error generating variant jobs for ${item.file_path} asset at ${asset}: ${error}`)
 	}
 
 	return jobs
