5dc03072bc2201e36c4a4eefd33e4533ccc2a552
eleith <eleith@noreply.git.eleith.com>
2025-10-03 09:04:05 -0700

    [tools] migrate to meta.url for resiliance after bundling

--- packages/tools/src/lib/config/config.ts
+++ packages/tools/src/lib/config/config.ts
@@ -1,5 +1,6 @@
 import { readFileSync, existsSync } from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { parse as yamlParse } from 'yaml'
 import { Ajv } from 'ajv'
 import { deepMerge } from '../deep-merge.js'
@@ -12,8 +13,9 @@ type ConfigPublic = {
 }
 
 function loadConfig(userConfigPath?: string): Config {
-	const configPath = path.join(import.meta.dirname, 'defaults.yaml')
-	const schemaPath = path.join(import.meta.dirname, 'schema.json')
+	const __dirname = path.dirname(fileURLToPath(import.meta.url))
+	const configPath = path.resolve(__dirname, 'defaults.yaml')
+	const schemaPath = path.resolve(__dirname, 'schema.json')
 	const schemaContent = readFileSync(schemaPath, 'utf-8')
 	const configContent = readFileSync(configPath, 'utf-8')
 	const schema = JSON.parse(schemaContent)
