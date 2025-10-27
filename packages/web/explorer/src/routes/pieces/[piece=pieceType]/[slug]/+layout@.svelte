fadae31227e38e31adc8a65c0109d381dc40aeae
eleith <online-gitea@eleith.com>
2025-10-27 10:50:06 -0700

    [web-explorer] custom page default

--- packages/web/explorer/src/routes/pieces/[piece=pieceType]/[slug]/+layout@.svelte
+++ packages/web/explorer/src/routes/pieces/[piece=pieceType]/[slug]/+layout@.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/state'
 	import Nav from '$lib/components/layout/simple/nav.svelte'
 
 	const { children } = $props()
 </script>
 
 <main>
-	{#if page.data.piece.media}
-		<Nav background="var(--colors-surface-container-lowest)" />
-	{:else}
-		<Nav />
-	{/if}
+	<Nav />
 	{@render children()}
 </main>
