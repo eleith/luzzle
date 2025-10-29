68fa8015b15506ae7ecbfbb7436e3edcf0524682
eleith <eleith@noreply.git.eleith.com>
2025-10-29 08:48:26 -0700

    [web-explorer] piece type restriction, release 0.0.93

--- packages/web/explorer/src/params/pieceType.ts
+++ packages/web/explorer/src/params/pieceType.ts
@@ -1,5 +1,8 @@
 import { type WebPieces } from '@luzzle/tools/types'
+import { PUBLIC_LUZZLE_PIECE_TYPES } from '$env/static/public'
+
+const luzzleTypes = PUBLIC_LUZZLE_PIECE_TYPES.split(',')
 
 export function match(param: string): param is WebPieces['type'] {
-	return param.length > 0
+	return luzzleTypes.includes(param)
 }
