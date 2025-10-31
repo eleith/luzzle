95a34b3b878eb647640e3f21603a1c3c27b33522
eleith <eleith@noreply.git.eleith.com>
2025-10-31 08:02:24 -0700

    [core] fix markdown escape bug, better typing

--- packages/core/src/lib/markdown.test.ts
+++ packages/core/src/lib/markdown.test.ts
@@ -11,12 +11,31 @@ describe('./lib/markdown.ts', () => {
 		})
 	})
 
-	test('extract markdown and frontmatter', async () => {
+test('extract markdown and frontmatter', async () => {
 		const contents = '---\na: awesome\nb: banana\n---\nthis is markdown'
 
 		const extracted = await markdown.extractFullMarkdown(contents)
 
 		expect(extracted.frontmatter).toEqual({ a: 'awesome', b: 'banana' })
 		expect(extracted.markdown).toBe('this is markdown')
+	})
+
+	test('should not escape footnotes', async () => {
+		const contents = `--- 
+a: awesome
+b: banana
+---
+Here is a footnote reference,[^1] and another.[^longnote]
+
+[^1]: Here is the footnote.
+[^longnote]: Here's one with multiple blocks.
+`
+		const extracted = await markdown.extractFullMarkdown(contents)
+		expect(extracted.markdown).toBe(
+			`Here is a footnote reference,[^1] and another.[^longnote]
+
+[^1]: Here is the footnote.
+[^longnote]: Here's one with multiple blocks.`
+		)
 	})
 })
--- packages/core/src/lib/markdown.ts
+++ packages/core/src/lib/markdown.ts
@@ -1,45 +1,25 @@
-import { Transformer } from 'unified'
-import { VisitorResult } from 'unist-util-visit'
-import YAML from 'yaml'
+import { Root } from 'mdast';
 import { remark } from 'remark'
 import remarkFrontMatter from 'remark-frontmatter'
-import { visit, EXIT } from 'unist-util-visit'
-import { filter } from 'unist-util-filter'
-import { toMarkdown } from 'mdast-util-to-markdown'
-import { Nodes } from 'mdast'
+import { visit } from 'unist-util-visit'
+import YAML from 'yaml'
 
 async function extractFullMarkdown(
 	contents: string | Buffer
 ): Promise<{ frontmatter: { [key: string]: unknown }; markdown: string }> {
-	function extractFrontMatter(): Transformer {
-		const transformer: Transformer = (tree, vfile) => {
-			function visitor(node: { value: string }): VisitorResult {
-				vfile.data.frontmatter = YAML.parse(node.value)
-				return EXIT
-			}
+	let frontmatter = {}
+	let markdown = contents.toString()
 
-			visit(tree, 'yaml', visitor)
-		}
-		return transformer
-	}
-
-	function extractContent(): Transformer {
-		const plugin: Transformer = (tree, vfile) => {
-			const newTree = filter(tree, (node) => node.type !== 'yaml')
-			vfile.data.content = newTree ? toMarkdown(newTree as Nodes) : ''
-		}
-		return plugin
-	}
-
-	const { data } = await remark()
+	await remark()
 		.use(remarkFrontMatter)
-		.use(extractFrontMatter)
-		.use(extractContent)
+		.use(() => (tree: Root) => {
+			visit(tree, 'yaml', function (node) {
+				frontmatter = YAML.parse(node.value)
+				const endPosition = node.position?.end.offset
+				markdown = markdown.slice(endPosition).trim()
+			})
+		})
 		.process(contents)
-
-	const frontmatter = data.frontmatter as { [key: string]: unknown }
-	const content = data.content as string
-	const markdown = content.trimEnd()
 
 	return { frontmatter, markdown }
 }
