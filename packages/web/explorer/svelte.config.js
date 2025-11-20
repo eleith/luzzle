11e50933f9d4b49c4811f5ef6694f509c409fbe4
eleith <eleith@noreply.git.eleith.com>
2025-11-20 07:50:23 -0800

    [web-explorer] migrate to web.tools and web.utils

--- packages/web/explorer/svelte.config.js
+++ packages/web/explorer/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { loadConfig } from '@luzzle/tools'
+import { loadConfig } from '@luzzle/web.utils/server'
 
 const config = loadConfig('./config.yaml')
 
