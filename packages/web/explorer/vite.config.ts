f549eb9f1567a52f9e77477c2d66b49e65653ec6
eleith <online-gitea@eleith.com>
2025-12-05 10:08:19 -0800

    [web.explorer] watch for content/ changes in dev

--- packages/web/explorer/vite.config.ts
+++ packages/web/explorer/vite.config.ts
@@ -1,8 +1,33 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin, type ViteDevServer } from 'vite'
 import Icons from 'unplugin-icons/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
+import path from 'path'
+import { execSync } from 'child_process'
+
+const contentWatcher = (relativeContentPath: string): Plugin => {
+	return {
+		name: 'content-watcher',
+		apply: 'serve',
+		configureServer(server: ViteDevServer) {
+			const targetPath = path.resolve(process.cwd(), relativeContentPath)
+			console.log(`[Content Watcher] Watching: ${targetPath}`)
+			server.watcher.add(targetPath)
+			server.watcher.on('change', (path) => {
+				if (path.includes(targetPath)) {
+					console.log('Content changed, syncing...')
+					execSync('npm run sync:content')
+				}
+			})
+		}
+	}
+}
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit(), Icons({ compiler: 'svelte' })]
+	plugins: [
+		enhancedImages(),
+		sveltekit(),
+		Icons({ compiler: 'svelte' }),
+		contentWatcher('./content')
+	]
 })
