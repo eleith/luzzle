6c908d4cb293bd1c5984b64140c86112a47e4168
eleith <eleith@noreply.git.eleith.com>
2026-01-25 13:01:51 -0800

    feat(web-explorer): setup authjs with oidc, editor route guard, and custom logos

--- packages/web/explorer/src/app.d.ts
+++ packages/web/explorer/src/app.d.ts
@@ -1,12 +1,16 @@
 import { type AppConfigPublic } from '$lib/server/config'
+import type { Session } from '@auth/core/types'
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth(): Promise<Session | null>
+		}
 		interface PageData {
+			session: Session | null
 			config: AppConfigPublic
 			meta: {
 				title?: string
--- packages/web/explorer/src/hooks.server.ts
+++ packages/web/explorer/src/hooks.server.ts
@@ -1,2 +1,40 @@
-import '$lib/server/config'
+import { config } from '$lib/server/config'
 import '$lib/server/database'
+import { SvelteKitAuth } from '@auth/sveltekit'
+import { sequence } from '@sveltejs/kit/hooks'
+import { redirect, type Handle } from '@sveltejs/kit'
+
+const authHandle = SvelteKitAuth({
+	trustHost: true,
+	secret: config.auth.secret,
+	providers: [
+		{
+			id: 'oidc',
+			name: 'OIDC',
+			type: 'oidc',
+			issuer: config.auth.oidc.issuer,
+			clientId: config.auth.oidc.clientId,
+			clientSecret: config.auth.oidc.clientSecret,
+			style: {
+				logo: `${config.url.app_assets}/images/favicon.png`
+			}
+		}
+	],
+})
+
+const guardHandle: Handle = async ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/editor')) {
+		if (!config.auth.enabled) {
+			throw redirect(302, '/')
+		}
+
+		const session = await event.locals.auth()
+		if (!session) {
+			throw redirect(302, '/auth/signin')
+		}
+	}
+
+	return resolve(event)
+}
+
+export const handle = sequence(authHandle.handle, guardHandle)
