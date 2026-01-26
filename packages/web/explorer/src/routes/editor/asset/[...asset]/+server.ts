d79e030388fa0f5a238a3fba2393271b35e2f7bc
eleith <eleith@noreply.git.eleith.com>
2026-01-26 07:21:08 -0800

    refactor(web-explorer): remove redundant auth checks in editor routes

--- packages/web/explorer/src/routes/editor/asset/[...asset]/+server.ts
+++ packages/web/explorer/src/routes/editor/asset/[...asset]/+server.ts
@@ -3,18 +3,8 @@ import type { RequestHandler } from './$types'
 import { getStorage } from '$lib/server/storage'
 import mime from 'mime-types'
 import { Readable } from 'stream'
-import { redirect } from '@sveltejs/kit'
-import { config } from '$lib/server/config'
 
 export const GET: RequestHandler = async ({ params, locals }) => {
-	// Guard: Ensure user is authenticated
-	if (config.auth.enabled) {
-		const session = await locals.auth()
-		if (!session) {
-			throw redirect(302, '/auth/signin')
-		}
-	}
-
 	const assetPath = path.normalize(params.asset)
 	const storage = getStorage()
 
