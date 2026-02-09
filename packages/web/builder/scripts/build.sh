a8097a41b2deb83c143bea4f784282adb4648f4d
eleith <eleith@noreply.git.eleith.com>
2026-02-08 18:03:08 -0800

    [web.builder] simplify flags

--- packages/web/builder/scripts/build.sh
+++ packages/web/builder/scripts/build.sh
@@ -4,16 +4,16 @@ echo "[build] started at $(date)"
 bash /app/scripts/pre.build.sh
 
 echo "Syncing archive to database..."
-luzzle-web-tools sync --config /app/config.yaml
+luzzle-web-tools sync --in /app/archive
 
 echo "Building database web tables"
-luzzle-web-tools sqlite --config /app/config.yaml
+luzzle-web-tools sqlite
 
 echo "Building new assets..."
-luzzle-web-tools assets --config /app/config.yaml --out /app/assets
+luzzle-web-tools assets --out /app/assets/pieces
 
 echo "Building new opengraph images..."
-luzzle-web-tools opengraph --config /app/config.yaml --out /app/assets
+luzzle-web-tools opengraph --out /app/assets/pieces
 
 bash /app/scripts/post.build.sh
 
