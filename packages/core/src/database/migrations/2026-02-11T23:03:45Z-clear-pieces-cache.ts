13b54ea71e78385a11185e8b3c43c4ec1396e8c8
eleith <eleith@noreply.git.eleith.com>
2026-02-14 09:24:48 -0800

    [core] redo migration to avoid destroying previous ids

--- packages/core/src/database/migrations/2026-02-11T23:03:45Z-clear-pieces-cache.ts
+++ packages/core/src/database/migrations/2026-02-11T23:03:45Z-clear-pieces-cache.ts
@@ -2,7 +2,7 @@ import { Kysely } from 'kysely'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function up(db: Kysely<any>): Promise<void> {
-	await db.deleteFrom('pieces_items').execute()
+	await db.deleteFrom('pieces_cache').execute()
 }
 
 export async function down(): Promise<void> {
