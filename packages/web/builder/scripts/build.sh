0b3dcc3c63fba21bcdbb1ed0efc280a8f8e1eea9
eleith <eleith@noreply.git.eleith.com>
2026-03-02 10:34:18 -0800

    [web.builder] only call sync

--- packages/web/builder/scripts/build.sh
+++ packages/web/builder/scripts/build.sh
@@ -6,9 +6,6 @@ bash /app/scripts/pre.build.sh
 echo "Syncing archive to database..."
 luzzle-web-tools sync --in /app/archive
 
-echo "Building database web tables"
-luzzle-web-tools sqlite
-
 echo "Building new assets..."
 luzzle-web-tools assets --out /app/assets/pieces --in /app/archive
 
