93a8e1a06955b7d91bf18d9cec72b0bac46b21d2
eleith <eleith@noreply.git.eleith.com>
2026-03-03 09:56:56 -0800

    [web] transform route for opengraph, icon

---
+++ packages/web/explorer/src/routes/api/pieces/[piece=pieceType]/[slug]/transform/icon/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import PieceIcon from '$lib/pieces/components/icon.svelte'
+	import { setContext } from 'svelte'
+
+	let { data } = $props()
+	setContext('piece-mode', 'local')
+</script>
+
+<PieceIcon piece={data.piece} size={{ width: 500 }} lazy={false} />
---
+++ packages/web/explorer/src/routes/api/pieces/[piece=pieceType]/[slug]/transform/opengraph/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Opengraph from '$lib/pieces/components/opengraph.svelte'
+	import { setContext } from 'svelte'
+
+	let { data } = $props()
+	setContext('piece-mode', 'local')
+</script>
+
+<Opengraph piece={data.piece} palette={data.palette} />
