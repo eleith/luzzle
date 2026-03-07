374743a2b6a69c9d407a9624d02136161b072198
eleith <eleith@noreply.git.eleith.com>
2026-03-06 16:48:07 -0800

    [web] remove old apis that are in transform route

--- packages/web/explorer/src/routes/api/pieces/[piece=pieceType]/[slug]/icon/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import PieceIcon from '$lib/pieces/components/icon.svelte'
-	import { setContext } from 'svelte'
-
-	let { data } = $props()
-	setContext('piece-mode', data.mode)
-</script>
-
-<PieceIcon piece={data.piece} metadata={data.piece.metadata} size={{ width: 500 }} lazy={false} />
--- packages/web/explorer/src/routes/api/pieces/[piece=pieceType]/[slug]/opengraph/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Opengraph from '$lib/pieces/components/opengraph.svelte'
-	import { setContext } from 'svelte'
-
-	let { data } = $props()
-	setContext('piece-mode', data.mode)
-</script>
-
-<Opengraph piece={data.piece} />
