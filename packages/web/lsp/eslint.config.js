73c3f1631a8e5a6634813115bed7aad70c8b6bd5
eleith <eleith@noreply.git.eleith.com>
2026-03-28 04:54:23 -0700

    [web.lsp] add markdownlint dependencies and convert configs to ESM
    Add markdownlint, vscode-languageserver, and
    vscode-languageserver-textdocument. Convert eslint and vitest configs
    from CJS to ESM to match package.json "type": "module".

--- packages/web/lsp/eslint.config.js
+++ packages/web/lsp/eslint.config.js
@@ -1,8 +1,8 @@
-const js = require('@eslint/js')
-const prettier = require('eslint-config-prettier')
-const globals = require('globals')
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
 
-module.exports = [
+export default [
 	js.configs.recommended,
 	prettier,
 	{
