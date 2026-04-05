f6a33306294cd6af454a6fbef6f81b1f3e8d46ac
eleith <eleith@noreply.git.eleith.com>
2026-04-05 07:41:25 -0700

    [web] revert to static

--- packages/web/explorer/src/lib/pieces/helpers.ts
+++ packages/web/explorer/src/lib/pieces/helpers.ts
@@ -1,11 +1,11 @@
 import { type WebPieceTags } from '@luzzle/web.utils'
 import { page } from '$app/state'
-import { env } from '$env/dynamic/public'
+import { PUBLIC_LUZZLE_PIECE_TYPES } from '$env/static/public'
 import type { PublicWebPiece } from './types'
 import type { PieceFrontMatterValue } from '@luzzle/core'
 
 export function getPieceTypes(): string[] {
-	return (env.PUBLIC_LUZZLE_PIECE_TYPES ?? '').split(',').filter(Boolean)
+	return (PUBLIC_LUZZLE_PIECE_TYPES ?? '').split(',').filter(Boolean)
 }
 
 export type PieceComponentHelpers = {
