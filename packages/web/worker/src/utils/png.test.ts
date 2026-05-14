782e5389f379708b6aec1cc94754bea3d83b1aed
eleith <eleith@noreply.git.eleith.com>
2026-05-14 10:12:12 -0700

    [web.worker] add browser and png utils

---
+++ packages/web/worker/src/utils/png.test.ts
@@ -0,0 +1,58 @@
+import { describe, test, vi, afterEach, expect } from 'vitest';
+import { generatePngFromUrl } from './png.js';
+import { Browser } from 'puppeteer';
+
+vi.mock('puppeteer');
+
+describe('lib/utils/png', () => {
+	const mocks = {
+		newPage: vi.fn(),
+		setViewport: vi.fn(),
+		setContent: vi.fn(),
+		goto: vi.fn(),
+		screenshot: vi.fn(),
+		close: vi.fn(),
+	};
+
+	afterEach(() => {
+		Object.values(mocks).forEach((mock) => {
+			mock.mockReset();
+		});
+	});
+
+	test('should generate a PNG image', async () => {
+		mocks.screenshot.mockResolvedValue(Buffer.from('test'));
+		const browser = {
+			newPage: mocks.newPage.mockResolvedValue({
+				setViewport: mocks.setViewport,
+				goto: mocks.goto,
+				screenshot: mocks.screenshot,
+				close: mocks.close,
+			}),
+		} as unknown as Browser;
+
+		const imageBuffer = await generatePngFromUrl('http://localhost', browser, '');
+
+		expect(imageBuffer).toBeDefined();
+		expect(mocks.newPage).toHaveBeenCalledOnce();
+		expect(mocks.setViewport).toHaveBeenCalledOnce();
+		expect(mocks.goto).toHaveBeenCalledOnce();
+		expect(mocks.screenshot).toHaveBeenCalledOnce();
+		expect(mocks.close).toHaveBeenCalledOnce();
+	});
+
+	test('should throw an error if screenshot fails', async () => {
+		mocks.screenshot.mockRejectedValue(new Error('test error'));
+		const browser = {
+			newPage: mocks.newPage.mockResolvedValue({
+				setViewport: mocks.setViewport,
+				goto: mocks.goto,
+				screenshot: mocks.screenshot,
+				close: mocks.close,
+			}),
+		} as unknown as Browser;
+
+		await expect(generatePngFromUrl('http://localhost', browser, '')).rejects.toThrowError()
+		expect(mocks.close).toHaveBeenCalledOnce();
+	});
+});
---
+++ packages/web/worker/src/utils/png.ts
@@ -0,0 +1,32 @@
+import { mkdir } from 'fs/promises'
+import path from 'path'
+import { Browser } from 'puppeteer'
+
+async function generatePngFromUrl(
+	url: string,
+	browser: Browser,
+	outputFilePath: string,
+) {
+	const page = await browser.newPage()
+	const directory = path.dirname(outputFilePath)
+
+	try {
+		await page.setViewport({
+			width: 1200,
+			height: 630,
+		})
+		await page.goto(url, { waitUntil: 'networkidle0' })
+	  await mkdir(directory, { recursive: true })
+
+		return await page.screenshot({
+			path: `${outputFilePath.replace(/\.png$/, '')}.png`,
+		})
+	} catch (error) {
+		throw new Error(`Failed to generate image with Puppeteer: ${error}`)
+	/* v8 ignore next line */
+	} finally {
+		await page.close()
+	}
+}
+
+export { generatePngFromUrl }
