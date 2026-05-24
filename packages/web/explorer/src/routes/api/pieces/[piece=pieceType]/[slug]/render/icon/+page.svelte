aaab3e93647c268e28e795b4756c336186d7668e
eleith <eleith@noreply.git.eleith.com>
2026-05-24 07:07:12 -0700

    [web.explorer] remove legacy public render API routes
    These routes rendered icon/opengraph by importing Svelte compilation
    into SvelteKit. Replaced by the worker-driven preview pipeline.

--- packages/web/explorer/src/routes/api/pieces/[piece=pieceType]/[slug]/render/icon/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import PieceIcon from '$lib/pieces/components/icon.svelte'
-
-	let { data } = $props()
-</script>
-
-<PieceIcon piece={data.piece} size={{ width: 500 }} lazy={false} mode="local" />
--- packages/web/explorer/src/routes/api/pieces/[piece=pieceType]/[slug]/render/opengraph/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Opengraph from '$lib/pieces/components/opengraph.svelte'
-
-	let { data } = $props()
-</script>
-
-<Opengraph piece={data.piece} mode="local" />
