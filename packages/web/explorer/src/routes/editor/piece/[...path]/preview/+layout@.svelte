608d69bfd8f702b3db7ecd55ee69cb10fc1fcf1e
eleith <eleith@noreply.git.eleith.com>
2026-05-24 07:06:21 -0700

    [web.explorer] import base stylesheets in preview layout reset
    The preview layout uses @layout to reset inheritance, so it needs to
    explicitly import reset, base, markdown, and theme CSS.

--- packages/web/explorer/src/routes/editor/piece/[...path]/preview/+layout@.svelte
+++ packages/web/explorer/src/routes/editor/piece/[...path]/preview/+layout@.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import '@luzzle/web.theme/styles/reset.css'
+	import '@luzzle/web.theme/styles/base.css'
+	import '@luzzle/web.theme/styles/markdown.css'
+	import '$lib/ui/styles/theme.build.css'
+
 	const { children } = $props()
 </script>
 
